8f8bf1556d62839206616ed411bfe890999b4fbb
An Li <kidylee@gmail.com>
2022-08-31 19:34:08 +0800

    add s3

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,18 +8,6 @@ home = expanduser("~")
 
 
 class TestClass:
-    def test_no_configure(self):
-        runner = CliRunner()
-        if os.path.exists(f"{home}/.flow360/config.toml"):
-            os.remove(f"{home}/.flow360/config.toml")
-        from flow360.cli import flow360
-
-        result = runner.invoke(flow360, ["configure"], input="apikey")
-        assert result.exit_code == 0
-        with open(f"{home}/.flow360/config.toml", "r") as f:
-            config = toml.loads(f.read())
-            assert config
-
     def test_configure(self):
         runner = CliRunner()
         from flow360.cli import flow360
--- tests/test_http.py
+++ tests/test_http.py
@@ -17,13 +17,3 @@ def test_apikey_auth():
     r = requests.Request()
     api_key_auth(r)
     assert r.headers["simcloud-api-key"] == "apikey"
-
-
-def test_no_apikey_auth():
-    r = requests.Request()
-    if os.path.exists(f"{expanduser('~')}/.flow360/config.toml"):
-        os.remove(f"{expanduser('~')}/.flow360/config.toml")
-    with pytest.raises(
-        ValueError, match="API key not found, please set it by commandline: flow360 configure."
-    ):
-        api_key_auth(r)
