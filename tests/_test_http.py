a1da7c22e710477ebcf2850f9fabe115d3b7d1d0
An Li <kidylee@gmail.com>
2022-08-31 19:41:40 +0800

    add s3

--- tests/_test_http.py
+++ tests/_test_http.py
@@ -9,6 +9,16 @@ from flow360 import Env
 from flow360.cloud.http_util import api_key_auth, http
 
 
+def test_apikey_auth():
+    runner = CliRunner()
+    from flow360.cli import flow360
+
+    runner.invoke(flow360, ["configure"], input="apikey")
+    r = requests.Request()
+    api_key_auth(r)
+    assert r.headers["simcloud-api-key"] == "apikey"
+
+
 def test_no_apikey_auth():
     r = requests.Request()
     if os.path.exists(f"{expanduser('~')}/.flow360/config.toml"):
