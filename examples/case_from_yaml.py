36a5ede43d6cb30097caab9501e188263d39ea54
Maciej Skarysz <83596707+maciej-flexcompute@users.noreply.github.com>
2023-04-12 17:09:39 +0200

    update commets in examples

--- examples/case_from_yaml.py
+++ examples/case_from_yaml.py
@@ -3,7 +3,6 @@ from flow360.examples import OM6wing
 
 OM6wing.get_files()
 
-# submit mesh using mesh JSON from file
 volume_mesh = fl.VolumeMesh.from_file(OM6wing.mesh_filename, name="OM6wing-mesh")
 volume_mesh = volume_mesh.submit()
 print(volume_mesh)
--- examples/run_case_with_retry.py
+++ examples/run_case_with_retry.py
@@ -3,7 +3,7 @@ from flow360.examples import OM6wing
 
 OM6wing.get_files()
 
-# submit mesh with manual configuration
+# submit mesh
 volume_mesh = fl.VolumeMesh.from_file(OM6wing.mesh_filename, name="OM6wing-mesh")
 volume_mesh = volume_mesh.submit()
 print(volume_mesh)
