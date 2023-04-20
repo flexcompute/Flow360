bbceb6b139229840d7dbe4878a6172be363bda86
Maciej Skarysz <83596707+maciej-flexcompute@users.noreply.github.com>
2023-04-20 10:24:35 +0200

    Update README.md added setup.py fixed formatting

--- examples/run_case_from_files.py
+++ examples/run_case_from_files.py
@@ -5,7 +5,7 @@ OM6wing.get_files()
 
 # submit mesh
 volume_mesh = fl.VolumeMesh.from_file(OM6wing.mesh_filename, name="OM6wing-mesh")
-volume_mesh = volume_mesh.submit()
+# volume_mesh = volume_mesh.submit()
 print(volume_mesh)
 
 # # submit case using json file
