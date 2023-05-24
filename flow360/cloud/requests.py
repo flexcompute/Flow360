b03907977cddc4d9f6b83e2b609eb6c2cd411e8d
Maciej Skarysz <maciej@flexcompute.com>
2023-05-24 11:45:48 +0200

    fixed volume mesh upload with compression fixed style

---
+++ flow360/cloud/requests.py
@@ -0,0 +1,38 @@
+"""Requests module"""
+
+from typing import List, Optional, Union
+
+import pydantic as pd
+from typing_extensions import Literal
+
+from ..component.flow360_params.flow360_params import Flow360MeshParams
+
+
+class NewVolumeMeshRequest(pd.BaseModel):
+    """request for new volume mesh"""
+
+    name: str = pd.Field(alias="meshName")
+    file_name: str = pd.Field(alias="fileName")
+    tags: Optional[List[str]] = pd.Field(alias="meshTags")
+    format: Literal["aflr3", "cgns"] = pd.Field(alias="meshFormat")
+    endianness: Optional[Literal["little", "big"]] = pd.Field(alias="meshEndianness")
+    compression: Optional[Literal["gz", "bz2"]] = pd.Field(alias="meshCompression")
+    mesh_params: Optional[Flow360MeshParams] = pd.Field(alias="meshParams")
+    solver_version: Optional[str] = pd.Field(alias="solverVersion")
+
+    # pylint: disable=no-self-argument
+    @pd.validator("mesh_params")
+    def set_mesh_params(cls, value: Union[Flow360MeshParams, None]):
+        """validate mesh params"""
+        if value:
+            return value.to_flow360_json()
+        return value
+
+    def dict(self, *args, **kwargs) -> dict:
+        """returns dict representation of request"""
+        return super().dict(*args, by_alias=True, exclude_none=True, **kwargs)
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """config"""
+
+        allow_population_by_field_name = True
