e5c84c515585d341f06a8ef0b6735a0f833bb599
jinlin-flex <lin@flexcompute.com>
2023-06-08 09:55:26 -0400

    style check

--- tests/test_version_check.py
+++ tests/test_version_check.py
@@ -9,35 +9,30 @@ import flow360.version_check as vc
 def test_get_supported_server_versions():
     # Prepare mock data
     mock_response = MagicMock()
-    mock_response.__iter__.return_value = iter(
-        [{"version": "1.0.0"}, {"version": "2.0.3b5"}])
+    mock_response.__iter__.return_value = iter([{"version": "1.0.0"}, {"version": "2.0.3b5"}])
     # Mock the http_util.Http.get method
     with patch("flow360.version_check.http_util.Http.get") as mock_get:
         mock_get.return_value = mock_response
         # Test with a valid app_name
-        versions = vc.get_supported_server_versions(
-            "flow360-python-client-v2")
+        versions = vc.get_supported_server_versions("flow360-python-client-v2")
         # Add appropriate assertions based on the expected behavior
         assert versions == ["1.0.0", "2.0.3b5"]
 
         # Test with an HTTPError
         mock_get.side_effect = requests.exceptions.HTTPError()
         with pytest.raises(requests.exceptions.HTTPError):
-            vc.get_supported_server_versions(
-                "invalid-app_name")
+            vc.get_supported_server_versions("invalid-app_name")
 
         # Check if the expected error message is raised
         with pytest.raises(requests.exceptions.HTTPError) as exc_info:
-            vc.get_supported_server_versions(
-                "flow360-python-client-v2")
+            vc.get_supported_server_versions("flow360-python-client-v2")
         assert str(exc_info.value) == "Error in connecting server"
 
     # Test with no version
     mock_response.__iter__.return_value = iter([])
     with patch("flow360.version_check.http_util.Http.get") as mock_get:
         with pytest.raises(RuntimeError) as exc_info:
-            vc.get_supported_server_versions(
-                "flow360-python-client-v2")
+            vc.get_supported_server_versions("flow360-python-client-v2")
         assert str(exc_info.value) == "Error in fetching supported versions"
 
 
@@ -64,8 +59,7 @@ def test_check_client_version():
         with patch(
             "flow360.version_check.get_supported_server_versions", return_value=supported_versions
         ):
-            version_status, version = vc.check_client_version(
-                "flow360-python-client-v2")
+            version_status, version = vc.check_client_version("flow360-python-client-v2")
             assert version_status == vc.VersionSupported.YES
             assert str(version) == current_version
 
@@ -75,8 +69,7 @@ def test_check_client_version():
         with patch(
             "flow360.version_check.get_supported_server_versions", return_value=supported_versions
         ):
-            version_status, version = vc.check_client_version(
-                "flow360-python-client-v2")
+            version_status, version = vc.check_client_version("flow360-python-client-v2")
             assert version_status == vc.VersionSupported.NO
             assert str(version) == current_version
 
@@ -86,8 +79,7 @@ def test_check_client_version():
         with patch(
             "flow360.version_check.get_supported_server_versions", return_value=supported_versions
         ):
-            version_status, version = vc.check_client_version(
-                "flow360-python-client-v2")
+            version_status, version = vc.check_client_version("flow360-python-client-v2")
             assert version_status == vc.VersionSupported.CAN_UPGRADE
             assert str(version) == latest_version
 
@@ -139,24 +131,21 @@ def test_client_version_get_info(capsys):
     # Test VersionSupported.NO
     with patch("flow360.version_check.check_client_version") as mock_check_client_version:
         with pytest.raises(SystemExit) as exc_info:
-            mock_check_client_version.return_value = (
-                vc.VersionSupported.NO, "1.0")
+            mock_check_client_version.return_value = (vc.VersionSupported.NO, "1.0")
             vc.client_version_get_info("solver")
             captured = capsys.readouterr()
             assert "Your version of CLI (1.0) is no longer supported." in captured.out
 
     # Test VersionSupported.CAN_UPGRADE
     with patch("flow360.version_check.check_client_version") as mock_check_client_version:
-        mock_check_client_version.return_value = (
-            vc.VersionSupported.CAN_UPGRADE, "2.0")
+        mock_check_client_version.return_value = (vc.VersionSupported.CAN_UPGRADE, "2.0")
         vc.client_version_get_info("app_name")
         captured = capsys.readouterr()
         assert "New version of CLI (2.0) is now available." in captured.out
 
     # Test VersionSupported.YES
     with patch("flow360.version_check.check_client_version") as mock_check_client_version:
-        mock_check_client_version.return_value = (
-            vc.VersionSupported.YES, "1.0")
+        mock_check_client_version.return_value = (vc.VersionSupported.YES, "1.0")
         vc.client_version_get_info("app_name")
         captured = capsys.readouterr()
         assert captured.out == ""
