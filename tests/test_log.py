905a35cec7e56efa0557abdd7c06b1025151403c
jinlin-flex <lin@flexcompute.com>
2023-06-13 13:32:37 -0400

    add log test for log with arguments

--- tests/test_log.py
+++ tests/test_log.py
@@ -5,22 +5,26 @@ set_logging_level("DEBUG")
 
 def test_debug():
     log.debug("Debug log")
+    log.debug("Debug log string %s， number %d", "arg", 1)
 
 
 def test_info():
     log.info("Basic info")
+    log.info("Basic info string %s， number %d", "arg", 1)
 
 
 def test_warning():
     log.warning("Warning log")
+    log.warning("Warning log string %s， number %d", "arg", 1)
 
 
 def test_error():
     log.error("Error log")
+    log.error("Error log string %s， number %d", "arg", 1)
 
 
 def test_critical():
-    log.critical("Critical log")
+    log.critical("Critical log string %s， number %d", "arg", 1)
 
 
 test_debug()
