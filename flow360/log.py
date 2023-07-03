cfdf2954f5d5ef67cbb5ca5e8055e33fd9ded804
jinlin-flex <106976695+jinlin-flex@users.noreply.github.com>
2023-07-03 09:30:19 -0400

    Revert "change logging messages to white"
    This reverts commit f52e559294aee21b575a2621e29e1a82e3c83360.

--- flow360/log.py
+++ flow360/log.py
@@ -93,25 +93,25 @@ class Logger:
     def info(self, message: str, *args) -> None:
         """Log (message) % (args) at info level"""
         message = message % args
-        message = f"[white]{message}[/white]"
+        message = f"[cyan]{message}[/cyan]"
         self._log(_level_value["INFO"], "INFO", message)
 
     def warning(self, message: str, *args) -> None:
         """Log (message) % (args) at warning level"""
         message = message % args
-        message = f"[white]{message}[/white]"
+        message = f"[yellow]{message}[/yellow]"
         self._log(_level_value["WARNING"], "WARNING", message)
 
     def error(self, message: str, *args) -> None:
         """Log (message) % (args) at error level"""
         message = message % args
-        message = f"[white]{message}[/white]"
+        message = f"[bold red]{message}[/bold red]"
         self._log(_level_value["ERROR"], "ERROR", message)
 
     def critical(self, message: str, *args) -> None:
         """Log (message) % (args) at critical level"""
         message = message % args
-        message = f"[white]{message}[/white]"
+        message = f"[bold underline red]{message}[/bold underline red]"
         self._log(_level_value["CRITICAL"], "CRITICAL", message)
 
 
