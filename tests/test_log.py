fb2301dfdc6219d56298a9549dfbeae79ba24ceb
jinlin-flex <106976695+jinlin-flex@users.noreply.github.com>
2023-07-03 09:30:19 -0400

    Revert "add log test for log with arguments"
    This reverts commit 905a35cec7e56efa0557abdd7c06b1025151403c.

--- tests/test_log.py
+++ tests/test_log.py
@@ -5,26 +5,22 @@ set_logging_level("DEBUG")
 
 def test_debug():
     log.debug("Debug log")
-    log.debug("Debug log string %s， number %d", "arg", 1)
 
 
 def test_info():
     log.info("Basic info")
-    log.info("Basic info string %s， number %d", "arg", 1)
 
 
 def test_warning():
     log.warning("Warning log")
-    log.warning("Warning log string %s， number %d", "arg", 1)
 
 
 def test_error():
     log.error("Error log")
-    log.error("Error log string %s， number %d", "arg", 1)
 
 
 def test_critical():
-    log.critical("Critical log string %s， number %d", "arg", 1)
+    log.critical("Critical log")
 
 
 test_debug()
