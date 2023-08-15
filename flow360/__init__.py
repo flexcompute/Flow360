849d08213005b01079a2b11638f0c885476c3d60
Maciej Skarysz <maciej@flexcompute.com>
2023-08-15 14:53:59 +0200

    download: removed keep_folder  when used with to_folder

--- flow360/__init__.py
+++ flow360/__init__.py
@@ -3,8 +3,7 @@ This module is flow360.
 """
 
 
-from flow360 import global_exception_handler  # This sets the global exception handler
-
+from . import global_exception_handler
 from .cli import flow360
 from .cloud.s3_utils import ProgressCallbackInterface
 from .component import meshing
