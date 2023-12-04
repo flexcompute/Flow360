054ff7e99b32ab4befe48d2e9ffd01164064edb6
Maciej Skarysz <maciej@flexcompute.com>
2023-12-04 15:30:50 +0100

    added validation handling

--- flow360/services.py
+++ flow360/services.py
@@ -1,3 +1,4 @@
+import pydantic as pd
 from .component.flow360_params.flow360_params import Flow360Params, Geometry, NavierStokesSolver, TurbulenceModelSolverSA, FreestreamFromVelocity
 
 
@@ -32,3 +33,38 @@ def get_default_retry(params_as_dict):
 def get_default_fork(params_as_dict):
     params = Flow360Params(**params_as_dict)
     return params
+
+
+
+def validate_flow360_params_model(params_as_dict):
+
+    values, fields_set, validation_errors = pd.validate_model(Flow360Params, params_as_dict)
+    print(f'{values=}')
+    print(f'{fields_set=}')
+    print(f'{validation_errors=}')
+
+    # TODO:
+    # when validating freestream, errors from all Union options will be returned. Need to reduce number of validation errors:
+    # example when provided temperature -1
+    # validation_errors=ValidationError(model='Flow360Params', errors=[
+    # {'loc': ('freestream', 'Temperature'), 'msg': 'ensure this value is greater than 0', 'type': 'value_error.number.not_gt', 'ctx': {'limit_value': 0}}, 
+    # {'loc': ('freestream', 'Reynolds'), 'msg': 'field required', 'type': 'value_error.missing'}, 
+    # {'loc': ('freestream', 'Temperature'), 'msg': 'ensure this value is greater than 0', 'type': 'value_error.number.not_gt', 'ctx': {'limit_value': 0}}, 
+    # {'loc': ('freestream', 'mu_ref'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, 
+    # {'loc': ('freestream', 'velocity'), 'msg': 'field required', 'type': 'value_error.missing'}, 
+    # {'loc': ('freestream', 'Mach'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, 
+    # {'loc': ('freestream', 'mu_ref'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, 
+    # {'loc': ('freestream', 'temperature'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, 
+    # {'loc': ('freestream', 'Mach'), 'msg': 'unexpected value; permitted: 0', 'type': 'value_error.const', 'ctx': {'given': 0.5, 'permitted': (0,)}}, 
+    # {'loc': ('freestream', 'Mach_ref'), 'msg': 'field required', 'type': 'value_error.missing'}, 
+    # {'loc': ('freestream', 'Temperature'), 'msg': 'ensure this value is greater than 0', 'type': 'value_error.number.not_gt', 'ctx': {'limit_value': 0}}, 
+    # {'loc': ('freestream', 'velocity_ref'), 'msg': 'field required', 'type': 'value_error.missing'}, {'loc': ('freestream', 'Mach'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, 
+    # {'loc': ('freestream', 'mu_ref'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}, {'loc': ('freestream', 'temperature'), 'msg': 'extra fields not permitted', 'type': 'value_error.extra'}])
+    
+
+    validation_warnings = []
+
+    if validation_errors is not None:
+        return validation_errors.errors, validation_warnings
+
+    return None, validation_warnings
