6f3c1793605ec49e93d1e2c20a0fcd81646f6462
yifan-flex <yifan@flexcompute.com>
2023-12-08 16:37:47 +0000

    Fix unit test

--- tests/test_unit_system.py
+++ tests/test_unit_system.py
@@ -478,6 +478,7 @@ def test_unit_system_init():
         "density": {"value": 1.0, "units": "kg/m**3"},
         "viscosity": {"value": 1.0, "units": "Pa*s"},
         "angular_velocity": {"value": 1.0, "units": "rad/s"},
+        "heat_flux": {"value": 1.0, "units": "kg/s**3"},
     }
     us = fl.UnitSystem(**unit_system_dict)
     assert us == fl.SI_unit_system
