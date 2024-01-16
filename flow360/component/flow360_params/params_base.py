84c4983bb564a247d5358fae936df2c8e38a3c5c
awkrupka <146361853+awkrupka@users.noreply.github.com>
2024-01-16 15:17:32 +0100

    [FLPY-91] Fix option names + root property usage (#127)

--- flow360/component/flow360_params/params_base.py
+++ flow360/component/flow360_params/params_base.py
@@ -555,9 +555,7 @@ class Flow360BaseModel(BaseModel):
         cls._schema_remove_all(schema, "options")
 
     @classmethod
-    def flow360_schema(cls):
-        """Generate a schema json string for the flow360 model"""
-        schema = cls.schema()
+    def _schema_apply_root_property(cls, schema):
         if cls.SchemaConfig.root_property is not None:
             current = schema
             path = cls.SchemaConfig.root_property.split("/")
@@ -566,12 +564,18 @@ class Flow360BaseModel(BaseModel):
             schema[path[-1]] = current
             del schema["properties"]
             del schema["required"]
+
+    @classmethod
+    def flow360_schema(cls):
+        """Generate a schema json string for the flow360 model"""
+        schema = cls.schema()
+        cls._schema_apply_option_names(schema)
+        cls._schema_apply_root_property(schema)
         if cls.SchemaConfig.displayed is not None:
             schema["displayed"] = cls.SchemaConfig.displayed
         for item in cls.SchemaConfig.exclude_fields:
             cls._schema_remove(schema, item.split("/"))
         cls._schema_format_titles(schema)
-        cls._schema_apply_option_names(schema)
         cls._schema_fix_single_allof(schema)
         cls._schema_fix_single_value_enum(schema)
         for item in cls.SchemaConfig.optional_objects:
