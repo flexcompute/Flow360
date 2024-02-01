6df4e4314a6d3bbe5146f07dbfc1577899a642cf
NasserFlexCompute <nasser@flexcompute.com>
2024-02-01 17:06:55 -0500

    Make temperature optional

--- flow360/component/flow360_params/flow360_params.py
+++ flow360/component/flow360_params/flow360_params.py
@@ -542,7 +542,9 @@ class FreestreamFromMach(FreestreamBase):
     Mach: PositiveFloat = pd.Field(displayed="Mach number")
     Mach_ref: Optional[PositiveFloat] = pd.Field(alias="MachRef", displayed="Reference Mach number")
     mu_ref: PositiveFloat = pd.Field(alias="muRef", displayed="Dynamic viscosity [non-dim]")
-    temperature: PositiveFloat = pd.Field(alias="Temperature", displayed="Temperature [K]")
+    temperature: Optional[PositiveFloat] = pd.Field(
+        alias="Temperature", displayed="Temperature [K]"
+    )
 
     # pylint: disable=arguments-differ, unused-argument
     def to_solver(self, params: Flow360Params, **kwargs) -> FreestreamFromMach:
@@ -563,7 +565,9 @@ class FreestreamFromMachReynolds(FreestreamBase):
     Mach: PositiveFloat = pd.Field(displayed="Mach number")
     Mach_ref: Optional[PositiveFloat] = pd.Field(alias="MachRef", displayed="Reference Mach number")
     Reynolds: PositiveFloat = pd.Field(displayed="Reynolds number")
-    temperature: PositiveFloat = pd.Field(alias="Temperature", displayed="Temperature [K]")
+    temperature: Optional[PositiveFloat] = pd.Field(
+        alias="Temperature", displayed="Temperature [K]"
+    )
 
     # pylint: disable=arguments-differ, unused-argument
     def to_solver(self, params: Flow360Params, **kwargs) -> FreestreamFromMach:
@@ -582,7 +586,9 @@ class ZeroFreestream(FreestreamBase):
     Mach: Literal[0] = pd.Field(0, const=True, displayed="Mach number")
     Mach_ref: pd.confloat(gt=1.0e-12) = pd.Field(alias="MachRef", displayed="Reference Mach number")
     mu_ref: PositiveFloat = pd.Field(alias="muRef", displayed="Dynamic viscosity [non-dim]")
-    temperature: PositiveFloat = pd.Field(alias="Temperature", displayed="Temperature [K]")
+    temperature: Optional[PositiveFloat] = pd.Field(
+        alias="Temperature", displayed="Temperature [K]"
+    )
 
     # pylint: disable=arguments-differ, unused-argument
     def to_solver(self, params: Flow360Params, **kwargs) -> ZeroFreestream:
@@ -626,7 +632,10 @@ class FreestreamFromVelocity(FreestreamBase):
         mach = solver_values.pop("velocity")
         mach_ref = solver_values.pop("velocity_ref", None)
         mu_ref = solver_values.pop("viscosity")
-        temperature = solver_values.pop("temperature").to("K")
+
+        temperature = None
+        if solver_values.get("temperature") is not None:
+            temperature = solver_values.pop("temperature").to("K")
 
         if solver_values.get("model_type") is not None:
             solver_values.pop("model_type")
