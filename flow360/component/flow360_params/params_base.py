a8941b64a285f265e7cad2a34b7c69ffc8317edd
Maciej Skarysz <maciej@flexcompute.com>
2024-02-13 11:53:17 +0100

    fixed recursive set_will_export flag to support lists

--- flow360/component/flow360_params/params_base.py
+++ flow360/component/flow360_params/params_base.py
@@ -971,6 +971,12 @@ class Flow360BaseModel(BaseModel):
         for value in self.__dict__.values():
             if isinstance(value, Flow360BaseModel):
                 value.set_will_export_to_flow360(flag)
+            elif isinstance(value, list):
+                [
+                    item.set_will_export_to_flow360(flag)
+                    for item in value
+                    if isinstance(item, Flow360BaseModel)
+                ]
 
     def dict(self, *args, exclude=None, **kwargs) -> dict:
         """Returns dict representation of the model.
