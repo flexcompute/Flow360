e01ab5790769c20f7e3c2d92a0567aec23f78f77
Maciej Skarysz <maciej@flexcompute.com>
2024-03-06 16:04:40 +0100

    added copy mesh from example

---
+++ examples/run_case_from_example_mesh.py
@@ -0,0 +1,9 @@
+import flow360 as fl
+from flow360.examples import OM6wing
+
+vm = fl.VolumeMesh.copy_from_example("2ad77a88-1676-4f89-8652-13bd7e34f257")
+
+params = fl.Flow360Params(OM6wing.case_json)
+case = fl.Case.create("OM6wing", params, vm.id, solver_version="release-24.2")
+case = case.submit()
+print(case)
