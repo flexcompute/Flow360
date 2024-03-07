7268ae3854e43f44055de31e3b9bebf4c3f2c24d
Maciej Skarysz <maciej@flexcompute.com>
2024-03-07 20:10:52 +0100

    added update path

--- flow360/component/flow360_params/updater.py
+++ flow360/component/flow360_params/updater.py
@@ -12,8 +12,8 @@ def _no_update(params_as_dict):
 UPDATE_MAP = [
     ("0.2.0b16", "0.2.0b17", _no_update),
     ("0.2.0b17", "0.2.0b18", _no_update),
-    ("0.2.0b18", "23.3.1", _no_update),
-    ("23.3.1", "24.2.0b2", _no_update),
+    ("0.2.0b18", "23.3.2", _no_update),
+    ("23.3.2", "24.2.0b2", _no_update),
 ]
 
 
