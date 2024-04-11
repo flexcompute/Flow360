fb4ac9abe6f97587780093a97f1c177028f4cdf4
Feilin Jia <feilin@flexcompute.com>
2024-04-10 21:14:47 -0400

    fix

--- examples/beta_stl_volume_mesher_from_inputs.py
+++ examples/beta_stl_volume_mesher_from_inputs.py
@@ -1,19 +1,19 @@
 import flow360 as fl
 from flow360.component.meshing.params import VolumeMeshingParams, Volume,\
     Farfield
-
 import os
 os.environ["FLOW360_BETA_FEATURES"] = '1'
 
 surface_mesh = fl.SurfaceMesh.from_file(
-    geometry_file = "airplaneGeometry.stl", params=None, name="airplane-surface-mesh-beta"
+    surface_mesh_file = "airplaneGeometry.stl", name="airplane-surface-mesh-beta"
 )
 surface_mesh = surface_mesh.submit()
+
 print(surface_mesh)
 print(surface_mesh.params)
 
 params = fl.VolumeMeshingParams(
-    type = "beta",
+    type = "v2",
     volume = Volume(
         first_layer_thicknes=0.001,
         growth_rate=1.1,
@@ -22,5 +22,5 @@ params = fl.VolumeMeshingParams(
     farfield = Farfield(type="auto")
 )
 
-volume_mesh = surface_mesh.create_volume_mesh("airplane-volume-mesh-beta", params=params)
+volume_mesh = fl.VolumeMesh.create(surface_mesh_id=surface_mesh.id, name="airplane-volume-mesh-beta", params=params)
 volume_mesh = volume_mesh.submit()
