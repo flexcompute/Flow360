e4f0769e654dd7881a23056295068886c39f5f92
Feilin Jia <feilin@flexcompute.com>
2024-04-11 11:36:12 -0400

    add airplaneGeometry.stl to data
    improve volume mesher test

--- examples/volume_mesher_v2_from_inputs.py
+++ examples/volume_mesher_v2_from_inputs.py
@@ -1,12 +1,13 @@
 import os
 
+os.environ["FLOW360_BETA_FEATURES"] = "1"
 import flow360 as fl
 from flow360.component.meshing.params import Farfield, Volume, VolumeMeshingParams
 
-os.environ["FLOW360_BETA_FEATURES"] = "1"
+surface_mesh_stl = "../tests/data/surface_mesh/airplaneGeometry.stl"
 
 surface_mesh = fl.SurfaceMesh.from_file(
-    surface_mesh_file="airplaneGeometry.stl", name="airplane-surface-mesh-beta"
+    surface_mesh_file=surface_mesh_stl, name="airplane-surface-mesh-stl"
 )
 surface_mesh = surface_mesh.submit()
 
@@ -16,7 +17,7 @@ print(surface_mesh.params)
 params = fl.VolumeMeshingParams(
     type="v2",
     volume=Volume(
-        first_layer_thicknes=0.001,
+        first_layer_thickness=0.001,
         growth_rate=1.1,
         num_boundary_layers=2,
     ),
@@ -24,6 +25,6 @@ params = fl.VolumeMeshingParams(
 )
 
 volume_mesh = fl.VolumeMesh.create(
-    surface_mesh_id=surface_mesh.id, name="airplane-volume-mesh-beta", params=params
+    surface_mesh_id=surface_mesh.id, name="airplane-volume-mesh-from-stl", params=params
 )
 volume_mesh = volume_mesh.submit()
