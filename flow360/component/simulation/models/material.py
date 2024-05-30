321a8ebc2a207839545b7a209bcecb32ca1a319d
Yifan Bai <yifan@flexcompute.com>
2024-05-30 16:47:20 -0400

    Add material constants as property

--- flow360/component/simulation/models/material.py
+++ flow360/component/simulation/models/material.py
@@ -12,44 +12,55 @@ class MaterialBase(Flow360BaseModel):
     name: str = pd.Field()
 
 
-class Gas(MaterialBase):
-    name: Literal["gas"] = pd.Field("gas", frozen=True)
-    dynamic_viscosity: Union[ViscosityType, Sutherland] = pd.Field()
-    specific_heat_ratio: float = pd.Field()
-    gas_constant: GasConstantType = pd.Field()
-    prandtl_number: float = pd.Field()
-
-
 class Sutherland(Flow360BaseModel):
-    reference_viscosity: ViscosityType = pd.Field()
-    reference_temperature: TemperatureType = pd.Field()
-    effective_temperature: TemperatureType = pd.Field()
+    reference_viscosity: ViscosityType.Positive = pd.Field()
+    reference_temperature: TemperatureType.Positive = pd.Field()
+    effective_temperature: TemperatureType.Positive = pd.Field()
 
 
-class Air(Gas):
+class Air(Flow360BaseModel):
     name: Literal["air"] = pd.Field("air", frozen=True)
-    dynamic_viscosity: Union[ViscosityType, Sutherland] = pd.Field(
+    dynamic_viscosity: Union[ViscosityType.Positive, Sutherland] = pd.Field(
         Sutherland(
             reference_viscosity=1.716e-5, reference_temperature=273, effective_temperature=111
         )
     )
-    specific_heat_ratio: Literal["1.4"] = pd.Field("1.4", frozen=True)
-    gas_constant: Literal["287.0529"] = pd.Field("287.0529", frozen=True)
-    prandtl_number: Literal["0.72"] = pd.Field("0.72", frozen=True)
+
+    @property
+    def specific_heat_ratio(self) -> PositiveFloat:
+        # TODO: serialize
+        return 1.4
+
+    @property
+    def gas_constant(self) -> SpecificHeatType.Positive:
+        return 287.0529
+
+    @property
+    def prandtl_number(self) -> PositiveFloat:
+        return 0.72
 
 
 class Solid(MaterialBase):
     name: Literal["solid"] = pd.Field("solid", frozen=True)
-    thermal_conductivity: ThermalConductivityType = pd.Field()
-    density: Optional[DensityType] = pd.Field(None)
-    specific_heat_capacity: Optional[SpecificHeatCapacityType] = pd.Field(None)
+    thermal_conductivity: ThermalConductivityType.Positive = pd.Field()
+    density: Optional[DensityType.Positive] = pd.Field(None)
+    specific_heat_capacity: Optional[SpecificHeatCapacityType.Positive] = pd.Field(None)
 
 
-class Aluminum(Solid):
+class Aluminum(MaterialBase):
     name: Literal["Aluminum"] = pd.Field("Aluminum", frozen=True)
-    thermal_conductivity: Literal["235"] = pd.Field("235", frozen=True)
-    density: Literal["2710"] = pd.Field("2710", frozen=True)
-    specific_heat_capacity: Literal["903"] = pd.Field("903", frozen=True)
+
+    @property
+    def thermal_conductivity(self) -> ThermalConductivityType.Positive:
+        return 235
+
+    @property
+    def density(self) -> DensityType.Positive:
+        return 2710
+
+    @property
+    def specific_heat_capacity(self) -> SpecificHeatCapacityType.Positive:
+        return 903
 
 
 MaterialTypes = Union[Air, Solid, Aluminum]
