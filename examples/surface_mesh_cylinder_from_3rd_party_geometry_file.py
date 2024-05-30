507e9ee3548af2335e7e702a9378f1ab6e5f86dd
Feilin Jia <feilin@flexcompute.com>
2024-05-29 20:19:48 -0400

    fix surface mesh cylinder from 3rd party

--- examples/surface_mesh_cylinder_from_3rd_party_geometry_file.py
+++ examples/surface_mesh_cylinder_from_3rd_party_geometry_file.py
@@ -1,14 +1,16 @@
 import flow360 as fl
 from flow360.component.geometry import Geometry
-from flow360.examples import CylinderGeometry
+from flow360.examples import Cylinder
+
+fl.Env.preprod.active()
 
 geometry = Geometry.from_file(
-    CylinderGeometry.geometry,
+    Cylinder.geometry,
     name="testing-cylinder-geometry",
 )
 geometry = geometry.submit()
 
-params = fl.SurfaceMeshingParams(CylinderGeometry.surface_json)
+params = fl.SurfaceMeshingParams(Cylinder.surface_json)
 surface_mesh = fl.SurfaceMesh.create(
     geometry_id=geometry.id, params=params, name="cylinder-surface-mesh"
 )
