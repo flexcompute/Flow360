8887b1f2bb8f3b18b3631d7b90dc7d71c773037f
Yifan Bai <yifan@flexcompute.com>
2024-06-03 16:50:14 -0400

    Address comments

--- flow360/component/simulation/models/material.py
+++ flow360/component/simulation/models/material.py
@@ -1,13 +1,12 @@
 from typing import Literal, Optional, Union
 
 import pydantic as pd
-import unyt as u
 
+import flow360.component.simulation.units as u
 from flow360.component.simulation.framework.base_model import Flow360BaseModel
 from flow360.component.simulation.unit_system import (
     DensityType,
     HeatCapacityType,
-    SI_unit_system,
     TemperatureType,
     ThermalConductivityType,
     ViscosityType,
@@ -18,6 +17,7 @@ class MaterialBase(Flow360BaseModel):
     # Basic properties required to define a material.
     # For example: young's modulus, viscosity as an expression of temperature, etc.
     ...
+    type: str = pd.Field()
     name: str = pd.Field()
 
 
@@ -28,7 +28,8 @@ class Sutherland(Flow360BaseModel):
 
 
 class Air(Flow360BaseModel):
-    name: Literal["air"] = pd.Field("air", frozen=True)
+    type: Literal["air"] = pd.Field("air", frozen=True)
+    name: str = pd.Field("air")
     dynamic_viscosity: Union[ViscosityType.Positive, Sutherland] = pd.Field(
         Sutherland(
             reference_viscosity=1.716e-5 * u.Pa * u.s,
@@ -52,14 +53,19 @@ class Air(Flow360BaseModel):
 
 
 class SolidMaterial(MaterialBase):
-    name: Literal["solid"] = pd.Field("solid", frozen=True)
+    type: Literal["solid"] = pd.Field("solid", frozen=True)
+    name: str = pd.Field(frozen=True)
     thermal_conductivity: ThermalConductivityType.Positive = pd.Field(frozen=True)
     density: Optional[DensityType.Positive] = pd.Field(None, frozen=True)
     specific_heat_capacity: Optional[HeatCapacityType.Positive] = pd.Field(None, frozen=True)
 
 
-with SI_unit_system:
-    aluminum = SolidMaterial(thermal_conductivity=235, density=2710, specific_heat_capacity=903)
+aluminum = SolidMaterial(
+    name="aluminum",
+    thermal_conductivity=235 * u.kg / u.s**3 * u.m / u.K,
+    density=2710 * u.kg / u.m**3,
+    specific_heat_capacity=903 * u.m**2 / u.s**2 / u.K,
+)
 
 
 SolidMaterialTypes = SolidMaterial
