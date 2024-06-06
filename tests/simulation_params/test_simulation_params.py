91186da3e451048090fe8ecade56d2a4b588a5b1
benflexcompute <ben@flexcompute.com>
2024-06-06 00:27:29 +0000

    comments addressed

--- tests/simulation_params/test_simulation_params.py
+++ tests/simulation_params/test_simulation_params.py
@@ -3,12 +3,13 @@ import pytest
 import flow360.component.simulation.units as u
 from flow360.component.simulation.meshing_param.params import MeshingParameters
 from flow360.component.simulation.meshing_param.volume_params import UniformRefinement
+from flow360.component.simulation.models.surface_models import HeatFlux, SlipWall, Wall
 from flow360.component.simulation.models.volume_models import Fluid
 from flow360.component.simulation.operating_condition import (
     GenericReferenceCondition,
     ThermalState,
 )
-from flow360.component.simulation.primitives import Box, ReferenceGeometry
+from flow360.component.simulation.primitives import Box, ReferenceGeometry, Surface
 from flow360.component.simulation.simulation_params import SimulationParams
 from flow360.component.simulation.time_stepping.time_stepping import Steady
 from flow360.component.simulation.unit_system import CGS_unit_system
@@ -20,6 +21,8 @@ from tests.utils import to_file_from_file_test
 
 @pytest.mark.usefixtures("array_equality_override")
 def test_simulation_params_seralization():
+    my_wall_surface = Surface(name="my_wall")
+    my_slip_wall_surface = Surface(name="my_slip_wall")
     with CGS_unit_system:
         my_box = Box(
             name="my_box",
@@ -42,7 +45,16 @@ def test_simulation_params_seralization():
                 velocity_magnitude=234,
                 thermal_state=ThermalState(temperature=300 * u.K, density=1 * u.g / u.cm**3),
             ),
-            models=[Fluid()],
+            models=[
+                Fluid(),
+                Wall(
+                    entities=[my_wall_surface],
+                    use_wall_function=True,
+                    velocity=(1.0, 1.2, 2.4) * u.ft / u.s,
+                    heat_spec=HeatFlux(1.0 * u.W / u.m**2),
+                ),
+                SlipWall(entities=[my_slip_wall_surface]),
+            ],
             time_stepping=Steady(),
             user_defined_dynamics=[
                 UserDefinedDynamic(
