f639a897d541fff46d8e6e52bd84f6098a8a3462
benflexcompute <ben@flexcompute.com>
2024-06-07 20:18:34 +0000

    Added _type so translator knows the class

--- flow360/component/simulation/framework/single_attribute_base.py
+++ flow360/component/simulation/framework/single_attribute_base.py
@@ -9,5 +9,5 @@ from flow360.component.simulation.framework.base_model import Flow360BaseModel
 class SingleAttributeModel(Flow360BaseModel, metaclass=abc.ABCMeta):
     value: Any = pd.Field()
 
-    def __init__(self, value: Any):
-        super().__init__(value=value)
+    def __init__(self, value: Any, **kwargs):
+        super().__init__(value=value, **kwargs)
