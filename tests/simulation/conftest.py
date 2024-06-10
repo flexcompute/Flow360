6b20b75ecefd516fc68bfa25524e42d2b63d864b
benflexcompute <ben@flexcompute.com>
2024-06-10 16:16:17 +0000

    Part that should be cherry-picked-2

---
+++ tests/simulation/conftest.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta
+from flow360.component.simulation.framework.entity_registry import EntityRegistry
+from flow360.component.simulation.framework.entity_base import EntityBase
+
+
+class AssetBase(metaclass=ABCMeta):
+    _registry: EntityRegistry
+
+    def __init__(self):
+        self._registry = EntityRegistry()
+
+    def __getitem__(self, key: str) -> list[EntityBase]:
+        """Use [] to access the registry"""
+        if isinstance(key, str) == False:
+            raise ValueError(f"Entity naming pattern: {key} is not a string.")
+        found_entities = self._registry.find_by_name_pattern(key)
+        if found_entities == []:
+            raise ValueError(
+                f"Failed to find any matching entity with {key}. Please check your input."
+            )
+        return found_entities
---
+++ tests/simulation/translator/test_surface_meshing_translator.py
@@ -0,0 +1,82 @@
+from flow360.component.simulation.translator.surface_mesh_translator import get_surface_mesh_json
+from flow360.component.simulation.simulation_params import SimulationParams
+import pytest
+from flow360.component.simulation.meshing_param.params import MeshingParams
+from flow360.component.simulation.meshing_param.face_params import SurfaceRefinement
+from flow360.component.simulation.meshing_param.edge_params import (
+    SurfaceEdgeRefinement,
+    HeightBasedRefinement,
+)
+from tests.simulation.conftest import AssetBase
+from flow360.component.simulation.primitives import Edge, Surface
+import flow360.component.simulation.units as u
+from flow360.component.simulation.unit_system import SI_unit_system, LengthType
+
+
+class TempGeometry(AssetBase):
+    """Mimicing the final VolumeMesh class"""
+
+    fname: str
+    mesh_unit: LengthType.Positive
+
+    def _get_meta_data(self):
+        if self.fname == "om6wing.csm":
+            return {
+                "edges": {
+                    "wingLeadingEdge": {},
+                    "wingTrailingEdge": {},
+                    "rootAirfoilEdge": {},
+                    "tipAirfoilEdge": {},
+                },
+                "surfaces": {"wing": {}},
+                "mesh_unit": {"units": "m", "value": 1.0},
+            }
+        else:
+            raise ValueError("Invalid file name")
+
+    def _populate_registry(self):
+        self.mesh_unit = LengthType.validate(self._get_meta_data()["mesh_unit"])
+        for zone_name in self._get_meta_data()["edges"]:
+            self._registry.register(Edge(name=zone_name))
+        for surface_name in self._get_meta_data()["surfaces"]:
+            self._registry.register(Surface(name=surface_name))
+
+    def __init__(self, file_name: str):
+        super().__init__()
+        self.fname = file_name
+        self._populate_registry()
+
+
+@pytest.fixture()
+def get_geometry():
+    return TempGeometry("om6wing.csm")
+
+
+@pytest.fixture()
+def get_test_param():
+    my_geometry = TempGeometry("om6wing.csm")
+    with SI_unit_system:
+        param = SimulationParams(
+            meshing=MeshingParams(
+                surface_layer_growth_rate=1.07,
+                refinements=[
+                    SurfaceRefinement(
+                        entities=[my_geometry["wing"]],
+                        max_edge_length=15 * u.cm,
+                        curvature_resolution_angle=10 * u.deg,
+                    ),
+                    SurfaceEdgeRefinement(
+                        entities=[my_geometry["wing*Edge"]],
+                        method=HeightBasedRefinement(value=3e-2 * u.cm),
+                    ),
+                ],
+            )
+        )
+    return param
+
+
+def test_param_to_json(get_test_param, get_geometry):
+    translated = get_surface_mesh_json(get_test_param, get_geometry.mesh_unit)
+    import json
+
+    print(json.dumps(translated, indent=4))
