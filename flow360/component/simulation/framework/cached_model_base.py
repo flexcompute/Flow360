8f691a8b7d8eff2693d6a7c879dbd12c4f1b5326
Yifan Bai <yifan@flexcompute.com>
2024-06-11 20:23:36 +0000

    Update cached model to store all constructor function arguments

--- flow360/component/simulation/framework/cached_model_base.py
+++ flow360/component/simulation/framework/cached_model_base.py
@@ -1,12 +1,35 @@
 import abc
-from typing import Any, Dict
+import inspect
+from functools import wraps
+from typing import Any, Callable, Dict
 
 import pydantic as pd
 
 from flow360.component.simulation.framework.base_model import Flow360BaseModel
+from flow360.component.types import TYPE_TAG_STR
 
 
 class CachedModelBase(Flow360BaseModel, metaclass=abc.ABCMeta):
+    @classmethod
+    def model_constructor(cls, func: Callable) -> Callable:
+        @classmethod
+        @wraps(func)
+        def wrapper(cls, *args, **kwargs):
+            sig = inspect.signature(func)
+            result = func(cls, *args, **kwargs)
+            defaults = {
+                k: v.default
+                for k, v in sig.parameters.items()
+                if v.default is not inspect.Parameter.empty
+            }
+            result._cached = result.__annotations__["_cached"](
+                **{**result._cached.model_dump(), **defaults, **kwargs}
+            )
+            result._cached.constructor = func.__name__
+            return result
+
+        return wrapper
+
     def __init__(self, **data):
         cached = data.pop("_cached", None)
         super().__init__(**data)
@@ -15,9 +38,17 @@ class CachedModelBase(Flow360BaseModel, metaclass=abc.ABCMeta):
                 self._cached = self.__annotations__["_cached"].model_validate(cached)
             except:
                 pass
+        else:
+            defaults = {name: field.default for name, field in self.__fields__.items()}
+            defaults.pop(TYPE_TAG_STR)
+            self._cached = self.__annotations__["_cached"](
+                **{**defaults, **data}, constructor="default"
+            )
 
     @pd.model_serializer(mode="wrap")
     def serialize_model(self, handler) -> Dict[str, Any]:
         serialize_self = handler(self)
-        serialize_self["_cached"] = self._cached.model_dump() if self._cached else None
+        serialize_self["_cached"] = (
+            self._cached.model_dump(exclude_none=True) if self._cached else None
+        )
         return serialize_self
