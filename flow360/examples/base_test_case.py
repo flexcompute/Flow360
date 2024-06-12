c586eba6446597fa9cbcdb318706f45e2aded7a9
Feilin Jia <feilin@flexcompute.com>
2024-06-12 14:34:41 +0000

    remove debug

--- flow360/examples/base_test_case.py
+++ flow360/examples/base_test_case.py
@@ -187,8 +187,6 @@ class BaseTestCase(metaclass=ABCMeta):
 
     @classmethod
     def _get_file(cls, remote_filename, local_filename):
-        print("debug ==== local filename = " + local_filename)
-        print("debug ==== remote filename = " + remote_filename)
         if not os.path.exists(local_filename):
             download(remote_filename, local_filename)
 
