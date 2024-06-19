1cd06470353d4c2f9c62d5d91d00edffdb5b97b4
Maciej Skarysz <maciej@flexcompute.com>
2024-06-19 14:54:25 +0800

    fix insolver translation service

--- flow360/component/simulation/translator/solver_translator.py
+++ flow360/component/simulation/translator/solver_translator.py
@@ -87,13 +87,13 @@ def get_solver_json(
             for surface in model.entities.stored_entities:
                 spec = dump_dict(model)
                 spec.pop("surfaces")
-            if isinstance(model, Wall):
-                spec.pop("useWallFunction")
-                spec["type"] = "WallFunction" if model.use_wall_function else "NoSlipWall"
-                if model.heat_spec:
-                    spec.pop("heat_spec")
-                    # TODO: implement
-            translated["boundaries"][surface.name] = spec
+                if isinstance(model, Wall):
+                    spec.pop("useWallFunction")
+                    spec["type"] = "WallFunction" if model.use_wall_function else "NoSlipWall"
+                    if model.heat_spec:
+                        spec.pop("heat_spec")
+                        # TODO: implement
+                translated["boundaries"][surface.name] = spec
 
     ##:: Step 4: Get outputs
     outputs = input_params.outputs
