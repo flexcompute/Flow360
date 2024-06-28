4de72c72381cafd992046e464fd8a59ca1981831
Maciej Skarysz <maciej@flexcompute.com>
2024-06-29 00:30:25 +0200

    added validation test for entities

--- tests/simulation/service/test_services_v2.py
+++ tests/simulation/service/test_services_v2.py
@@ -148,3 +148,12 @@ def test_validate_init_data_errors():
     _, errors, _ = services.validate_model(params_as_dict=data, unit_system_name="SI")
     json.dumps(errors)
     assert len(errors) == 1
+
+
+def test_validate():
+    params_str = '{"version":"24.3.0","unit_system":{"name":"SI"},"private_attribute_asset_cache":{"asset_entity_registry":{"internal_registry":{}}},"meshing":{"farfield":"auto","refinement_factor":1,"surface_layer_growth_rate":1.2,"refinements":[{"name":"Surface edge refinement 1","refinement_type":"SurfaceEdgeRefinement","_id":"1e36b5cf-23c6-499d-b2e1-12a2474853a3","type":"projectAnisoSpacing","method":"aspectRatio","value":-4,"edges":{"value":["iGf-HfizH8LFOFqa","8elVtrQVeW9CsFMD"]}},{"name":"Axisymmetric refinement 2","refinement_type":"AxisymmetricRefinement","_id":"153c5329-7d6c-40d6-9402-b02180afa41f"}],"volume_zones":[],"gap_treatment_strength":1},"operating_condition":{"type_name":"AerospaceCondition","private_attribute_constructor":"default","private_attribute_input_cache":{},"alpha":{"value":0,"units":"rad"},"beta":{"value":0,"units":"rad"},"thermal_state":{"type_name":"ThermalState","private_attribute_constructor":"default","private_attribute_input_cache":{},"temperature":{"value":288.15,"units":"K"},"density":{"value":1.225,"units":"kg/m**3"},"material":{"type":"air","name":"air","dynamic_viscosity":{"reference_viscosity":{"value":0.00001716,"units":"Pa*s"},"reference_temperature":{"value":273.15,"units":"K"},"effective_temperature":{"value":110.4,"units":"K"}}}},"velocity_magnitude":{"value":100,"units":"m/s"}},"time_stepping":{"order_of_accuracy":2,"type_name":"Steady","max_steps":700,"CFL":{"type":"adaptive","min":0.1,"max":10000,"max_relative_change":1,"convergence_limiting_factor":0.25}},"user_defined_dynamics":null,"outputs":null}'
+    _, errors, _ = services.validate_model(params_as_dict=json.loads(params_str), unit_system_name="SI")
+    json.dumps(errors)
+    assert len(errors) == 1
+
+
