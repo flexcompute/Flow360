1d0dd291c0eb054deecc16c909b4e811d121888a
Nasser Sabet <nasser@flexcompute.com>
2024-08-14 21:27:27 +0000

    Under beta flag

--- flow360/component/flow360_params/solvers.py
+++ flow360/component/flow360_params/solvers.py
@@ -11,6 +11,8 @@ import numpy as np
 import pydantic.v1 as pd
 from typing_extensions import Literal
 
+from flow360.flags import Flags
+
 from ..types import Coordinate
 from .flow360_legacy import (
     LegacyModel,
@@ -200,20 +202,22 @@ class NavierStokesSolver(GenericFlowSolverSettings):
     low_dissipation_control_factors: Optional[List[float]] = pd.Field(
         default=[], alias="lowDissipationControlFactors"
     )
-    debug_type: Optional[
-        Literal[
-            "point",
-            "minDensity",
-            "minPressure",
-            "maxVelocity",
-            "maxResCont",
-            "maxResMomX",
-            "maxResMomY",
-            "maxResMomZ",
-            "maxResEnergy",
-        ]
-    ] = pd.Field(alias="debugType")
-    debug_point: Optional[Coordinate] = pd.Field(alias="debugPoint")
+
+    if Flags.beta_features():
+        debug_type: Optional[
+            Literal[
+                "point",
+                "minDensity",
+                "minPressure",
+                "maxVelocity",
+                "maxResCont",
+                "maxResMomX",
+                "maxResMomY",
+                "maxResMomZ",
+                "maxResEnergy",
+            ]
+        ] = pd.Field(alias="debugType")
+        debug_point: Optional[Coordinate] = pd.Field(alias="debugPoint")
 
     # pylint: disable=arguments-differ,invalid-name
     def to_solver(self, params, **kwargs) -> NavierStokesSolver:
@@ -600,8 +604,6 @@ class NavierStokesSolverLegacy(NavierStokesSolver, LegacyModel):
             "lowDissipationControlFactors": self.low_dissipation_control_factors,
             "lowMachPreconditioner": self.low_mach_preconditioner,
             "lowMachPreconditionerThreshold": self.low_mach_preconditioner_threshold,
-            "debugType": self.debug_type,
-            "debugPoint": self.debug_point,
         }
 
         if self.linear_iterations is not None and model["linearSolverConfig"] is not None:
