382f8d22c562e3af3130ee90a603537d0526a170
BenYuan <ben@flexcompute.com>
2024-09-26 13:39:04 +0000

    Revert back the changes to examples

--- examples/geometry_to_surface_mesh_V2.py
+++ examples/geometry_to_surface_mesh_V2.py
@@ -1,5 +1,4 @@
 import flow360 as fl
-from flow360 import log
 from flow360.component.geometry import Geometry
 from flow360.component.simulation import cloud
 from flow360.component.simulation.meshing_param.face_params import (
@@ -17,23 +16,23 @@ from flow360.examples import Airplane
 
 fl.Env.dev.active()
 
-log.set_logging_level("DEBUG")
-
-geometry_draft = Geometry.from_file(Airplane.geometry, solver_version="workbench-24.9.2")
-geometry = geometry_draft.submit()
-# geometry = Geometry.from_cloud("geo-6e45ad20-74be-48d2-9092-d4de968df4f8")
-geometry.show_available_groupings(verbose_mode=True)
+# geometry_draft = Geometry.from_file(
+#     Airplane.geometry, solver_version="workbenchMeshGrouping-24.9.1"
+# )
+# geometry = geometry_draft.submit()
+geometry = Geometry.from_cloud("geo-e89fe565-24a2-4777-b563-2ec1d3d2a133")
+# geometry.show_available_groupings(verbose_mode=True)
 geometry.group_faces_by_tag(tag_name="groupName")
 with SI_unit_system:
     params = SimulationParams(
         meshing=MeshingParams(
             defaults=MeshingDefaults(
                 boundary_layer_first_layer_thickness=1,
-                surface_edge_growth_rate=1.41,
-                surface_max_edge_length=1.1111,
+                surface_edge_growth_rate=1.4,
+                surface_max_edge_length=1.0111,
             ),
             volume_zones=[AutomatedFarfield()],
         ),
     )
-cloud.generate_surface_mesh(geometry, params=params, draft_name="TestGrouping", async_mode=True)
+cloud.generate_surface_mesh(geometry, params=params, draft_name="TestGrouping", async_mode=False)
 # print(geometry._meta_class)
