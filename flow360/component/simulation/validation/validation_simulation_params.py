c4b3829b7d4d76f078d0f9e40e0078f78a98f48a
andrzej-krupka <156919532+andrzej-krupka@users.noreply.github.com>
2024-10-03 15:59:51 +0200

    Added check for low mach preconditioner sensor [SCFD-2210] (#443)
    * Added low mach preconditioner validator [SCFD-2209]
    
    * Addressed PR feedback
    
    * Addressed PR feedback
    
    * Style fixes

--- flow360/component/simulation/validation/validation_simulation_params.py
+++ flow360/component/simulation/validation/validation_simulation_params.py
@@ -9,17 +9,17 @@ from flow360.component.simulation.outputs.outputs import SurfaceOutput, VolumeOu
 
 
 def _check_consistency_wall_function_and_surface_output(v):
-    has_wall_function_model = False
-
     models = v.models
 
-    if models is None:
-        return v
+    if models:
+        has_wall_function_model = False
+        for model in models:
+            if isinstance(model, Wall) and model.use_wall_function:
+                has_wall_function_model = True
+                break
 
-    for model in models:
-        if isinstance(model, Wall) and model.use_wall_function:
-            has_wall_function_model = True
-            break
+        if has_wall_function_model:
+            return v
 
     outputs = v.outputs
 
@@ -28,31 +28,100 @@ def _check_consistency_wall_function_and_surface_output(v):
 
     for output in outputs:
         if isinstance(output, SurfaceOutput):
-            fields = output.output_fields.items
-            if "wallFunctionMetric" in fields and not has_wall_function_model:
+            if "wallFunctionMetric" in output.output_fields.items:
                 raise ValueError(
-                    "To use 'wallFunctionMetric' for output specify a Wall with use_wall_function=true"
+                    "To use 'wallFunctionMetric' for output specify a Wall model with use_wall_function=true. "
                 )
+
+    return v
+
+
+def _check_low_mach_preconditioner_output(v):
+    models = v.models
+
+    if models:
+        has_low_mach_preconditioner = False
+        for model in models:
+            if isinstance(model, Fluid) and model.navier_stokes_solver:
+                preconditioner = model.navier_stokes_solver.low_mach_preconditioner
+                if preconditioner:
+                    has_low_mach_preconditioner = True
+                    break
+
+        if has_low_mach_preconditioner:
+            return v
+
+    outputs = v.outputs
+
+    if not outputs:
+        return v
+
+    for output in outputs:
+        if not hasattr(output, "output_fields"):
+            continue
+        if "lowMachPreconditionerSensor" in output.output_fields.items:
+            raise ValueError(
+                "Low-Mach preconditioner output requested, but low_mach_preconditioner is not enabled. "
+                "You can enable it via model.navier_stokes_solver.low_mach_preconditioner = True for a Fluid "
+                "model in the models field of the simulation object."
+            )
+
+    return v
+
+
+def _check_numerical_dissipation_factor_output(v):
+    models = v.models
+
+    if models:
+        low_dissipation_enabled = False
+        for model in models:
+            if isinstance(model, Fluid) and model.navier_stokes_solver:
+                numerical_dissipation_factor = (
+                    model.navier_stokes_solver.numerical_dissipation_factor
+                )
+                low_dissipation_flag = int(round(1.0 / numerical_dissipation_factor)) - 1
+                if low_dissipation_flag != 0:
+                    low_dissipation_enabled = True
+                    break
+
+        if low_dissipation_enabled:
+            return v
+
+    outputs = v.outputs
+
+    if not outputs:
+        return v
+
+    for output in outputs:
+        if not hasattr(output, "output_fields"):
+            continue
+        if "numericalDissipationFactor" in output.output_fields.items:
+            raise ValueError(
+                "Numerical dissipation factor output requested, but low dissipation mode is not enabled. "
+                "You can enable it via model.navier_stokes_solver.numerical_dissipation_factor = True for a Fluid "
+                "model in the models field of the simulation object."
+            )
 
     return v
 
 
 def _check_consistency_ddes_volume_output(v):
     model_type = None
-    run_ddes = False
-
     models = v.models
 
-    if not models:
-        return v
+    run_ddes = False
 
-    for model in models:
-        if isinstance(model, Fluid):
-            turbulence_model_solver = model.turbulence_model_solver
-            if not isinstance(turbulence_model_solver, NoneSolver) and turbulence_model_solver.DDES:
-                model_type = turbulence_model_solver.type_name
-                run_ddes = True
-                break
+    if models:
+        for model in models:
+            if isinstance(model, Fluid):
+                turbulence_model_solver = model.turbulence_model_solver
+                if (
+                    not isinstance(turbulence_model_solver, NoneSolver)
+                    and turbulence_model_solver.DDES
+                ):
+                    model_type = turbulence_model_solver.type_name
+                    run_ddes = True
+                    break
 
     outputs = v.outputs
 
@@ -73,40 +142,5 @@ def _check_consistency_ddes_volume_output(v):
                 raise ValueError(
                     "kOmegaSST_DDES output can only be specified with kOmegaSST turbulence model and DDES turned on."
                 )
-
-    return v
-
-
-def _check_numerical_dissipation_factor_output(v):
-    models = v.models
-
-    if not models:
-        return v
-
-    low_dissipation_enabled = False
-
-    for model in models:
-        if isinstance(model, Fluid) and model.navier_stokes_solver:
-            numerical_dissipation_factor = model.navier_stokes_solver.numerical_dissipation_factor
-            low_dissipation_flag = int(round(1.0 / numerical_dissipation_factor)) - 1
-            if low_dissipation_flag != 0:
-                low_dissipation_enabled = True
-                break
-
-    if low_dissipation_enabled:
-        return v
-
-    outputs = v.outputs
-
-    if not outputs:
-        return v
-
-    for output in outputs:
-        if not hasattr(output, "output_fields"):
-            continue
-        if "numericalDissipationFactor" in output.output_fields.items:
-            raise ValueError(
-                "Numerical dissipation factor output requested, but low dissipation mode is not enabled"
-            )
 
     return v
