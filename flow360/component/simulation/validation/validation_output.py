a13cd2b1c22575821f80260ac7eb7333a1fcfe22
Angran Li <angran@flexcompute.com>
2024-10-08 20:50:28 +0000

    Ressolve Unit test issue

--- flow360/component/simulation/validation/validation_output.py
+++ flow360/component/simulation/validation/validation_output.py
@@ -1,10 +1,15 @@
-def _check_entities_type(value, ProbeOutputTypeStr: str):
+"""
+Validation for output parameters
+"""
+
+
+def _check_entities_type(value, probe_output_type_str: str):
     """check to ensure every entity has the same type"""
     if value is None:
         return value
     for entity in value.stored_entities:
         if type(entity) is not type(value.stored_entities[0]):
             raise ValueError(
-                f"All entities in a single `{ProbeOutputTypeStr}` must have the same type: `Point` or `PointArray`."
+                f"All entities in a single `{probe_output_type_str}` must have the same type: `Point` or `PointArray`."
             )
     return value
