97a3ca2877d403b2ac5c133b8a90a9700e02f0a0
Angran Li <angran@flexcompute.com>
2024-10-16 18:09:43 +0000

    Test edit on VolumeOutput

--- flow360/component/simulation/outputs/outputs.py
+++ flow360/component/simulation/outputs/outputs.py
@@ -97,10 +97,10 @@ class TimeAverageSurfaceOutput(SurfaceOutput):
 
 
 class VolumeOutput(_AnimationAndFileFormatSettings):
-    """Volume output settings."""
+    """Volume output settings. (test the code)"""
 
-    name: Optional[str] = pd.Field(None)
-    output_fields: UniqueItemList[VolumeFieldNames] = pd.Field()
+    name: Optional[str] = pd.Field(None, description="Name of this volume output.")
+    output_fields: UniqueItemList[VolumeFieldNames] = pd.Field(description="List of output variables.")
     output_type: Literal["VolumeOutput"] = pd.Field("VolumeOutput", frozen=True)
 
 
