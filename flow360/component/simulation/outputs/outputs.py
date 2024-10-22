e9803f7bf589b1fb47d178d6dafb0c6a6e1fc82e
Angran Li <angran@flexcompute.com>
2024-10-22 01:43:31 +0000

    First draft of output docs

--- flow360/component/simulation/outputs/outputs.py
+++ flow360/component/simulation/outputs/outputs.py
@@ -54,7 +54,7 @@ class _AnimationAndFileFormatSettings(_AnimationSettings):
     Controls how frequently the output files are generated and the file format.
     """
 
-    output_format: Literal["paraview", "tecplot", "both"] = pd.Field(default="paraview")
+    output_format: Literal["paraview", "tecplot", "both"] = pd.Field(default="paraview", description='"paraview", "tecplot" or "both".')
 
 
 class SurfaceOutput(_AnimationAndFileFormatSettings):
@@ -62,16 +62,16 @@ class SurfaceOutput(_AnimationAndFileFormatSettings):
 
     # pylint: disable=fixme
     # TODO: entities is None --> use all surfaces. This is not implemented yet.
-    name: Optional[str] = pd.Field(None)
-    entities: Optional[EntityList[Surface, GhostSurface]] = pd.Field(None, alias="surfaces")
+    name: Optional[str] = pd.Field(None, description='Name of the surface output.')
+    entities: Optional[EntityList[Surface, GhostSurface]] = pd.Field(None, alias="surfaces", description='List of output surfaces. The name of the surface is used as the key. These surface names have to be the patch name in the grid file or the alias name specified in case JSON.')
     write_single_file: bool = pd.Field(
         default=False,
         description="Enable writing all surface outputs into a single file instead of one file per surface."
         + "This option currently only supports Tecplot output format."
-        + "Will choose the value of the last instance of this option of the same output type"
+        + "Will choose the value of the last instance of this option of the same output type "
         + "(SurfaceOutput or TimeAverageSurfaceOutput) in the `output` list.",
     )
-    output_fields: UniqueAliasedStringList[SurfaceFieldNames] = pd.Field()
+    output_fields: UniqueAliasedStringList[SurfaceFieldNames] = pd.Field(description='List of output variables. Including :ref:`universal output variables<UniversalVariables>` and :ref:`variables specific to surfaceOutput<SurfaceSpecificVariables>`. :code:`outputFields` specified under :code:`surfaceOutput` will be added to all surfaces.')
     output_type: Literal["SurfaceOutput"] = pd.Field("SurfaceOutput", frozen=True)
 
 
@@ -96,10 +96,10 @@ class TimeAverageSurfaceOutput(SurfaceOutput):
 
 
 class VolumeOutput(_AnimationAndFileFormatSettings):
-    """Volume output settings. (test the code)"""
+    """Volume output settings."""
 
-    name: Optional[str] = pd.Field(None, description="Name of this volume output.")
-    output_fields: UniqueAliasedStringList[VolumeFieldNames] = pd.Field(description="List of output variables.")
+    name: Optional[str] = pd.Field(None, description="Name of the volume output.")
+    output_fields: UniqueAliasedStringList[VolumeFieldNames] = pd.Field(description="List of output variables. Including :ref:`universal output variables<UniversalVariables>`, and :ref:`variables specific to volumeOutput<VolumeAndSliceSpecificVariables>`.")
     output_type: Literal["VolumeOutput"] = pd.Field("VolumeOutput", frozen=True)
 
 
@@ -117,7 +117,7 @@ class TimeAverageVolumeOutput(VolumeOutput):
     """
 
     start_step: Union[pd.NonNegativeInt, Literal[-1]] = pd.Field(
-        default=-1, description="Physical time step to start calculating averaging"
+        default=-1, description="Physical time step to start calculating averaging."
     )
     output_type: Literal["TimeAverageVolumeOutput"] = pd.Field(
         "TimeAverageVolumeOutput", frozen=True
@@ -127,18 +127,18 @@ class TimeAverageVolumeOutput(VolumeOutput):
 class SliceOutput(_AnimationAndFileFormatSettings):
     """Slice output settings."""
 
-    name: Optional[str] = pd.Field(None)
-    entities: Optional[EntityList[Slice]] = pd.Field(None, alias="slices")
-    output_fields: UniqueAliasedStringList[SliceFieldNames] = pd.Field()
+    name: Optional[str] = pd.Field(None, description="Name of the slice output.")
+    entities: Optional[EntityList[Slice]] = pd.Field(None, alias="slices", description='List of output slice entities.')
+    output_fields: UniqueAliasedStringList[SliceFieldNames] = pd.Field(description='List of output variables. Including :ref:`universal output variables<UniversalVariables>` and :ref:`variables specific to sliceOutput<VolumeAndSliceSpecificVariables>`. :code:`outputFields` specified under :code:`sliceOutput` will be added to all slices.')
     output_type: Literal["SliceOutput"] = pd.Field("SliceOutput", frozen=True)
 
 
 class IsosurfaceOutput(_AnimationAndFileFormatSettings):
     """Isosurface output settings."""
 
-    name: Optional[str] = pd.Field(None)
-    entities: Optional[UniqueItemList[Isosurface]] = pd.Field(None, alias="isosurfaces")
-    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field()
+    name: Optional[str] = pd.Field(None, description='Name of the iso-surface output.')
+    entities: Optional[UniqueItemList[Isosurface]] = pd.Field(None, alias="isosurfaces", description= 'List of iso-surface entities.')
+    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field(description=' Isosurface field variable to be written. One of :code:`p`, :code:`rho`, :code:`Mach`, :code:`qcriterion`, :code:`s`, :code:`T`, :code:`Cp`, :code:`mut`, :code:`nuHat`.')
     output_type: Literal["IsosurfaceOutput"] = pd.Field("IsosurfaceOutput", frozen=True)
 
 
@@ -146,17 +146,17 @@ class SurfaceIntegralOutput(Flow360BaseModel):
     """Surface integral output settings."""
 
     name: str = pd.Field()
-    entities: Optional[EntityList[Surface, GhostSurface]] = pd.Field(None, alias="surfaces")
-    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field()
+    entities: Optional[EntityList[Surface, GhostSurface]] = pd.Field(None, alias="surfaces",description='List of surface entities on which the surface integral will be calculated.')
+    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field(description='List of output fields which will be added to all monitors within the monitor group, see universal output variables.')
     output_type: Literal["SurfaceIntegralOutput"] = pd.Field("SurfaceIntegralOutput", frozen=True)
 
 
 class ProbeOutput(Flow360BaseModel):
     """Probe monitor output settings."""
 
-    name: str = pd.Field()
-    entities: Optional[EntityList[Point]] = pd.Field(None, alias="probe_points")
-    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field()
+    name: str = pd.Field(description='Name of the monitor group.')
+    entities: Optional[EntityList[Point]] = pd.Field(None, alias="probe_points",description='List of monitored point entities belonging to this monitor group.')
+    output_fields: UniqueAliasedStringList[CommonFieldNames] = pd.Field(description='List of output fields which will be added to all monitors within the monitor group, see :ref:`universal output variables<UniversalVariables>`')
     output_type: Literal["ProbeOutput"] = pd.Field("ProbeOutput", frozen=True)
 
     def load_point_location_from_file(self, file_path: str):
@@ -170,8 +170,8 @@ class AeroAcousticOutput(Flow360BaseModel):
     name: Optional[str] = pd.Field(None)
     patch_type: Literal["solid"] = pd.Field("solid", frozen=True)
     # pylint: disable=no-member
-    observers: List[LengthType.Point] = pd.Field()
-    write_per_surface_output: bool = pd.Field(False)
+    observers: List[LengthType.Point] = pd.Field(description='List of observer locations at which time history of acoustic pressure signal is stored in aeroacoustic output file. The observer locations can be outside the simulation domain, but cannot be on or inside the solid surfaces of the simulation domain.')
+    write_per_surface_output: bool = pd.Field(False, description='Enable writing of aeroacoustic results on a per-surface basis, in addition to results for all wall surfaces combined.')
     output_type: Literal["AeroAcousticOutput"] = pd.Field("AeroAcousticOutput", frozen=True)
 
 
