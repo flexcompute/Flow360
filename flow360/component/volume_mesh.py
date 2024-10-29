18e87322344f6a5302fe27986e4fd3112b06aea3
angranl-flex <angran@flexcompute.com>
2024-10-29 17:00:07 -0400

    Add VolumeMesh from local storage and Update module import for local tests (#523) (#525)
    * Address comments to use new_simulation_models for importing modules
    
    * Create VolumeMesh object for localTests/Update import modules
    
    Fix uinit test
    
    * Address comments
    
    ---------
    
    Co-authored-by: Ben <106089368+benflexcompute@users.noreply.github.com>

--- flow360/component/volume_mesh.py
+++ flow360/component/volume_mesh.py
@@ -6,7 +6,9 @@ Volume mesh component
 
 from __future__ import annotations
 
+import json
 import os.path
+import shutil
 import threading
 from enum import Enum
 from typing import Any, Iterator, List, Optional, Union
@@ -27,6 +29,7 @@ from flow360.cloud.requests import (
     NewVolumeMeshRequestV2,
 )
 from flow360.cloud.rest_api import RestApi
+from flow360.cloud.s3_utils import get_local_filename_and_create_folders
 from flow360.component.compress_upload import compress_and_upload_chunks
 from flow360.exceptions import (
     Flow360CloudFileError,
@@ -1004,6 +1007,44 @@ class VolumeMeshV2(AssetBase):
         asset_obj = super().from_cloud(id)
 
         return asset_obj
+
+    @classmethod
+    def from_local_storage(cls, mesh_id: str = None, local_storage_path="") -> VolumeMeshV2:
+        """
+        Parameters
+        ----------
+        mesh_id : str
+            ID of the volume mesh resource
+
+        local_storage_path:
+            The folder of the project, defaults to current working directory
+
+        Returns
+        -------
+        VolumeMeshV2
+            Volume mesh object
+        """
+
+        def _local_download_file(
+            file_name: str,
+            to_file: str = None,
+            to_folder: str = ".",
+        ):
+            expected_local_file = os.path.join(local_storage_path, file_name)
+            if not os.path.exists(expected_local_file):
+                raise RuntimeError(
+                    f"File {expected_local_file} not found. Make sure the file exists when using "
+                    + "VolumeMeshV2.from_local_storage()."
+                )
+            new_local_file = get_local_filename_and_create_folders(file_name, to_file, to_folder)
+            if new_local_file != expected_local_file:
+                shutil.copy(expected_local_file, new_local_file)
+
+        _local_download_file(file_name="simulation.json", to_folder=local_storage_path)
+        with open(os.path.join(local_storage_path, "simulation.json"), encoding="utf-8") as f:
+            params_dict = json.load(f)
+        volume_mesh = super()._from_supplied_entity_info(params_dict, cls(mesh_id))
+        return volume_mesh
 
     @classmethod
     # pylint: disable=too-many-arguments
