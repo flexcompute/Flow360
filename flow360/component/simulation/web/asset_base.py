f7358d27f190e810de5bf48e6455b48283733f71
Feilin Jia <feilin@flexcompute.com>
2024-10-30 20:58:34 +0000

    fix file_names->file_name; fix compression strategy; fix remote file name fetching

--- flow360/component/simulation/web/asset_base.py
+++ flow360/component/simulation/web/asset_base.py
@@ -141,9 +141,6 @@ class AssetBase(metaclass=ABCMeta):
         :param tags:
         :return:
         """
-        if isinstance(file_names, str):
-            file_names = [file_names]
-        # pylint: disable=not-callable
         return cls._draft_class(
             file_names=file_names,
             project_name=project_name,
