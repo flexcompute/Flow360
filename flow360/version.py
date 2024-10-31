57f12a1394b639134107941d483d3aa1c8fd99b7
Maciej Skarysz <83596707+maciej-flexcompute@users.noreply.github.com>
2024-10-30 22:50:42 -0400

    bumped version to 24.2.6 (#535)

--- flow360/version.py
+++ flow360/version.py
@@ -2,4 +2,4 @@
 version
 """
 
-__version__ = "24.2.5"
+__version__ = "24.2.6"
--- tests/test_flow360.py
+++ tests/test_flow360.py
@@ -2,4 +2,4 @@ from flow360 import __version__
 
 
 def test_version():
-    assert __version__ == "24.2.5"
+    assert __version__ == "24.2.6"
