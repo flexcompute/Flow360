515cf1ea13850341d9b5ee25da3e4d494f3e7248
Maciej Skarysz <maciej@flexcompute.com>
2024-10-31 11:47:59 -0400

    added comments for assigning _id after submission

--- flow360/component/geometry.py
+++ flow360/component/geometry.py
@@ -202,6 +202,7 @@ class GeometryDraft(ResourceDraft):
         ##:: kick off pipeline
         geometry._webapi._complete_upload()
         log.info(f"Geometry successfully submitted: {geometry.short_description()}")
+        # setting _id will disable "remember to submit draft" warning message
         self._id = info.id
         log.info("Waiting for geometry to be processed.")
         # uses from_cloud to ensure all metadata is ready before yielding the object
