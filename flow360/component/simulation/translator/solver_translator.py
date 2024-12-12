ec245b8b9c593bc9cdc1936f72cbfd0d939ffe0e
BenYuan <ben@flexcompute.com>
2024-12-12 19:55:57 +0000

    Added validation

--- flow360/component/simulation/translator/solver_translator.py
+++ flow360/component/simulation/translator/solver_translator.py
@@ -459,7 +459,10 @@ def translate_acoustic_output(output_params: list):
             aeroacoustic_output["observers"] = [item.value.tolist() for item in output.observers]
             aeroacoustic_output["writePerSurfaceOutput"] = output.write_per_surface_output
             aeroacoustic_output["patchType"] = output.patch_type
-            aeroacoustic_output["permeableSurfaces"] = output.permeable_surfaces
+            if output.permeable_surfaces is not None:
+                aeroacoustic_output["permeableSurfaces"] = [
+                    item.full_name for item in output.permeable_surfaces.stored_entities
+                ]
             return aeroacoustic_output
     return None
 
