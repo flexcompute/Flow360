a6977fcafd160c553b25c89a4d78b6759f84e3f9
Angran <angran@flexcompute.com>
2025-01-06 14:10:13 -0500

    Support the mix of Point and PointArray in ProbeOutput and SurfaceProbeOutput (#640) (#641)
    * Support the mix of Point and PointArray in ProbeOutput
    
    * Modify class examples and address comments
    
    * Remove _check_unique_probe_type function
    
    * Improve class examples

--- flow360/component/simulation/validation/validation_output.py
+++ flow360/component/simulation/validation/validation_output.py
@@ -5,18 +5,6 @@ Validation for output parameters
 from typing import List, Literal, Union, get_args, get_origin
 
 
-def _check_unique_probe_type(value, probe_output_type_str: str):
-    """check to ensure every entity has the same type"""
-    if value is None:
-        return value
-    for entity in value.stored_entities:
-        if type(entity) is not type(value.stored_entities[0]):
-            raise ValueError(
-                f"All entities in a single `{probe_output_type_str}` must have the same type: `Point` or `PointArray`."
-            )
-    return value
-
-
 def _check_output_fields(params):
     """Check the specified output fields for each output item is valid."""
 
