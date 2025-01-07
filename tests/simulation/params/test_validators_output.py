7efd7429767f54842cb321bb50087b67aeb4ac96
BenYuan <ben@flexcompute.com>
2025-01-07 19:33:15 +0000

    Fix unit test

--- tests/simulation/params/test_validators_output.py
+++ tests/simulation/params/test_validators_output.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 import flow360.component.simulation.units as u
@@ -17,7 +19,9 @@ def test_aeroacoustic_output_observer_converter():
 def test_aeroacoustic_observer_unit_validator():
     with pytest.raises(
         ValueError,
-        match="All observer locations should have the same unit. But now it has both `cm` and `mm`.",
+        match=re.escape(
+            "All observer locations should have the same unit. But now it has both `cm` and `mm`."
+        ),
     ):
         AeroAcousticOutput(
             name="test", observers=[[0.2, 0.02, 0.03] * u.cm, [0.0001, 0.02, 0.03] * u.mm]
