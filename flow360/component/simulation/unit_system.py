d5cc647c556932cdd1bc144c883296cc6e9c53c8
BenYuan <ben@flexcompute.com>
2025-01-23 16:20:18 +0000

    Revert "[SCFD-4128] [SCFD-4126] feat(): Removed validation process for unit system conversion (#666) (#679)"
    This reverts commit 2568a161bc6e0fe6ea36a9f83f054e8bcbdaa8f3.

--- flow360/component/simulation/unit_system.py
+++ flow360/component/simulation/unit_system.py
@@ -123,8 +123,7 @@ def _dimensioned_type_serializer(x):
     """
     encoder for dimensioned type (unyt_quantity, unyt_array, DimensionedType)
     """
-    # adding .expr helps to avoid degF/C becoming serialized as °F/C
-    return {"value": _encode_ndarray(x.value), "units": str(x.units.expr)}
+    return {"value": _encode_ndarray(x.value), "units": str(x.units)}
 
 
 def _check_if_input_has_delta_unit(quant):
@@ -931,10 +930,6 @@ class _Flow360BaseUnit(_DimensionedType):
 
             def __str__(self):
                 return f"{parent_self.unit_name}"
-
-            def expr(self):
-                """alias for __str__ so the serializer can work"""
-                return str(self)
 
         return _Units()
 
