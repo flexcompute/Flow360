dae9acc901511833b3e83bb4a62be2daa3e3a43a
wiktor-flex <wiktor.krzew@flexcompute.com>
2025-01-24 10:21:07 +0100

    fix to import in test_bet_translator

--- tests/simulation/translator/test_bet_translator.py
+++ tests/simulation/translator/test_bet_translator.py
@@ -6,7 +6,7 @@ import unittest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import flow360 as fl
-from tests.utils import compare_values
+from flow360.component.simulation.framework.updater_utils import compare_values
 
 assertions = unittest.TestCase("__init__")
 
