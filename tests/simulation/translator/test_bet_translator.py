3fa0b7a29914628a0e18a2682e417f076a20a3b7
wiktor-flex <wiktor.krzew@flexcompute.com>
2025-01-24 10:56:42 +0100

    deleted sys.path from unittest

--- tests/simulation/translator/test_bet_translator.py
+++ tests/simulation/translator/test_bet_translator.py
@@ -1,9 +1,6 @@
 import json
 import os
-import sys
 import unittest
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import flow360 as fl
 from flow360.component.simulation.framework.updater_utils import compare_values
