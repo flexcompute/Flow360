fd29895640cf12b38991ae9298a597e1586478f5
Jim <jim@flexcompute.com>
2025-01-30 08:01:20 -0700

    one more round of changes, added log.info instead of print

--- examples/retrieve_results/template_script_download_results.py
+++ examples/retrieve_results/template_script_download_results.py
@@ -2,18 +2,15 @@
 Download volume, surface, acoustic and other types of output files to the directory it is run from.
 """
 
-
 import os
 import tarfile
 import timeit
-
 import click
-
+from flow360.log import log
 import flow360 as fl
 
+# Enter case specific settings here.
 case_id = "ENTER CASE ID HERE"
-case_id = 'case-789f852a-529a-4e9e-b394-d8d8bdcd15e5'
-# Used only if we are interested in downloading specific data sets
 download_surfaces = True
 download_volumes = True
 
@@ -29,14 +26,10 @@ if os.path.exists(destination):
         default=True,
         abort=True,
     )
-print('Beginning downloading')
-
+log.info("Beginning downloading")
 # Download only specific data sets
 results.download(
-    surface=download_surfaces,
-    volume=download_volumes,
-    destination=destination,
-    overwrite=True
+    surface=download_surfaces, volume=download_volumes, destination=destination, overwrite=True
 )
 
 # Extract tar.gz files
@@ -44,13 +37,13 @@ tar_gz_files = [f for f in os.listdir(destination) if f.endswith(".tar.gz")]
 for tar_gz_file in tar_gz_files:
     start = timeit.default_timer()
     file_path = os.path.join(destination, tar_gz_file)
-    print(f"Processing: {file_path}")
+    log.info(f"Processing: {file_path}")
 
     with tarfile.open(file_path, "r:gz") as tar:
         result_name = tar_gz_file[:-7]
         tar.extractall(path=os.path.join(destination, result_name))
 
     os.remove(file_path)
-    print(f"  Removed: {file_path}")
-    print(f"Extracting files for {tar_gz_file} done")
-print('Downloading successful')
+    log.info(f"  Removed: {file_path}")
+    log.info(f"Extracting files for {tar_gz_file} done")
+log.info("Downloading successful")
