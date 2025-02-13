07ba698d495232502a514798be18537c3fd4f237
wiktor-flex <wiktor.krzew@flexcompute.com>
2025-02-13 16:13:56 +0100

    examples adjustments

---
+++ examples/client_utils/case_list.py
@@ -0,0 +1,11 @@
+import flow360.v1 as fl
+
+from flow360.log import log
+
+for case in fl.MyCases(limit=10000):
+    log.info(
+        "\n" + "Case ID: " + str(case.id) +
+        "\n" + "Case name: " + str(case.name) +
+        "\n" + "Status: " + str(case.status.value) +
+        "\n" + "Solver version: " + str(case.solver_version)
+    )
