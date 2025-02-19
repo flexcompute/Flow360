28eb3a1bdf34aaa594ab207492e01f857692f029
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-02-18 22:11:45 -0500

    changed omega dimensionalization for bet converter (#736) (#747)
    * time_unit -> freestream_temperature
    
    ---------
    
    Co-authored-by: wiktor-flex <wiktor.krzew@flexcompute.com>

--- examples/migration_guide/example_bet_conversion.py
+++ examples/migration_guide/example_bet_conversion.py
@@ -1,5 +1,6 @@
 import os
 
+import flow360 as fl
 from flow360.component.simulation.migration import BETDisk
 from flow360.component.simulation.unit_system import u
 
@@ -8,14 +9,18 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Change the current working directory to the script directory
 os.chdir(script_dir)
 
+with fl.SI_unit_system:
+    params = fl.SimulationParams(operating_condition=fl.AerospaceCondition(velocity_magnitude=10))
+
 my_BETDisk = BETDisk.read_single_v1_BETDisk(
     file_path="./BET_tutorial_Flow360.json",
     mesh_unit=u.m,
-    time_unit=u.s,
+    freestream_temperature=params.operating_condition.thermal_state.temperature,
 )
 
 print(my_BETDisk)
 
+# Converted BETDisk can be used in a simulation
 """
 with SI_unit_system:
     params = fl.SimulationParams(
@@ -28,3 +33,9 @@ with SI_unit_system:
         ...
     )
 """
+
+# After creating params, changing the value of omega can be done by doing the following
+"""
+params.models[X].omega = 500 * fl.u.rpm
+"""
+# where X is an int specifying chosen BETDisk's position in the list of models
