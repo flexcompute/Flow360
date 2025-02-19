06fb024c8d4473dc39dada2f777702df9780a925
wiktor-flex <wiktor.krzew@flexcompute.com>
2025-02-19 11:56:20 +0100

    adjusted case_list.py

--- examples/client_utils/case_list.py
+++ examples/client_utils/case_list.py
@@ -2,17 +2,4 @@ import flow360.v1 as fl
 from flow360.log import log
 
 for case in fl.MyCases(limit=10000):
-    log.info(
-        "\n"
-        + "Case ID: "
-        + str(case.id)
-        + "\n"
-        + "Case name: "
-        + str(case.name)
-        + "\n"
-        + "Status: "
-        + str(case.status.value)
-        + "\n"
-        + "Solver version: "
-        + str(case.solver_version)
-    )
+    log.info(case.short_description() + "solver_version = " + str(case.solver_version))
