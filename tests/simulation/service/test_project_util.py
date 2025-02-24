367395cba25b2d2db7d9334efa807c0d0fcb3cf4
Angran <angran@flexcompute.com>
2025-02-24 16:50:55 -0500

    [SCFD-4474] Add validation before draft submission (#761)
    * Add params validation before submitting the draft
    
    * Update validation func's name
    
    * Update error message print
    
    * Revert change in services
    
    * Implement validation function in project_utils and format the output message.
    
    * Fix black
    
    * Address comments

--- tests/simulation/service/test_project_util.py
+++ tests/simulation/service/test_project_util.py
@@ -1,6 +1,6 @@
 import pytest
 
-from flow360.component.project_utils import replace_ghost_surfaces
+from flow360.component.project_utils import _replace_ghost_surfaces
 from flow360.component.simulation.primitives import GhostSphere
 from flow360.component.simulation.simulation_params import SimulationParams
 
@@ -12,7 +12,7 @@ def change_test_dir(request, monkeypatch):
 
 def test_replace_ghost_surfaces():
     params = SimulationParams.from_file("./data/simulation_with_old_ghost_surface.json")
-    new_params = replace_ghost_surfaces(params)
+    new_params = _replace_ghost_surfaces(params)
     entity = new_params.models[1].entities.stored_entities[0]
     assert isinstance(entity, GhostSphere)  # This used to be GhostSurface by python
     assert entity.center == [5.0007498695, 0.0, 0.0]
