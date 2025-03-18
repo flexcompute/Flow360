e9867a20cf25848c932f8874d778285c64240485
Maciej Skarysz <maciej@flexcompute.com>
2025-03-18 20:42:09 +0100

    fixed test

--- tests/simulation/framework/test_unique_list.py
+++ tests/simulation/framework/test_unique_list.py
@@ -87,7 +87,7 @@ def test_unique_list():
     with pytest.raises(
         ValueError,
         match=re.escape(
-            "Input should be 'Cp', 'Cpt', 'gradW', 'kOmega', 'Mach', 'mut', 'mutRatio', 'nuHat', 'primitiveVars', 'qcriterion', 'residualNavierStokes', 'residualTransition', 'residualTurbulence', 's', 'solutionNavierStokes', 'solutionTransition', 'solutionTurbulence', 'T', 'vorticity', 'vorticityMagnitude', 'wallDistance', 'numericalDissipationFactor', 'residualHeatSolver', 'VelocityRelative' or 'lowMachPreconditionerSensor'"
+            "Input should be 'Cp', 'Cpt', 'gradW', 'kOmega', 'Mach', 'mut', 'mutRatio', 'nuHat', 'primitiveVars', 'qcriterion', 'residualNavierStokes', 'residualTransition', 'residualTurbulence', 's', 'solutionNavierStokes', 'solutionTransition', 'solutionTurbulence', 'T', 'velocity', 'velocity_magnitude', 'pressure', 'vorticity', 'vorticityMagnitude', 'wallDistance', 'numericalDissipationFactor', 'residualHeatSolver', 'VelocityRelative', 'lowMachPreconditionerSensor', 'velocity_m_per_s', 'velocity_magnitude_m_per_s', 'velocity_x_m_per_s', 'velocity_y_m_per_s', 'velocity_z_m_per_s', 'pressure_pa', 'wall_shear_stress_magnitude' or 'wall_shear_stress_magnitude_pa'"
         ),
     ):
         TempIsosurfaceOutput(isosurfaces=[my_iso_1], output_fields=["wallDistance", 1234])
