8b9102f0507b64a2abe49cebd5ecf05937aaf195
Malhar Prajapati <168122498+malhardp333@users.noreply.github.com>
2025-03-28 14:42:23 -0400

    Shorten the quickstart tutorial (#868)

--- examples/tutorials/quick_start.py
+++ examples/tutorials/quick_start.py
@@ -3,15 +3,8 @@ import flow360 as fl
 from flow360.examples import eVTOL
 from flow360.log import log
 
-solver_version = "release-24.11"
-
 # Create a project and upload the geometry
-project = fl.Project.from_file(
-    eVTOL.geometry,
-    name="EVTOL_quickstart_from_API",
-    length_unit="m",
-    solver_version=solver_version,
-)
+project = fl.Project.from_file(eVTOL.geometry, name="EVTOL_quickstart_from_API", length_unit="m")
 
 log.info(f"The project id is {project.id}")
 
@@ -42,8 +35,6 @@ with fl.SI_unit_system:
                 )
             ],
         ),
-        # Set the reference geometry.
-        reference_geometry=fl.ReferenceGeometry(),
         # Set the operating condition.
         operating_condition=fl.AerospaceCondition(
             velocity_magnitude=50 * fl.u.m / fl.u.s, alpha=0 * fl.u.deg
@@ -56,10 +47,8 @@ with fl.SI_unit_system:
             fl.Wall(surfaces=geometry["fuselage"], name="fuselage"),
             # Notice below how we use the * operator to assign both left and right pylon to the pylons subcomponent
             fl.Wall(surfaces=geometry["*_pylon"], name="pylons"),
-            fl.Wall(surfaces=geometry["left_wing"], name="left_wing"),
-            fl.Wall(surfaces=geometry["right_wing"], name="right_wing"),
-            fl.Wall(surfaces=geometry["h_tail"], name="h_tail"),
-            fl.Wall(surfaces=geometry["v_tail"], name="v_tail"),
+            fl.Wall(surfaces=geometry["*_wing"], name="wing"),
+            fl.Wall(surfaces=geometry["*_tail"], name="tail"),
             fl.Freestream(surfaces=[farfield_zone.farfield], name="Freestream"),
         ],
         # Output format could be 'paraview' or 'tecplot' or 'both'.
@@ -74,22 +63,5 @@ with fl.SI_unit_system:
         ],
     )
 # Run the case.
-project.run_case(
-    params=params,
-    solver_version=solver_version,
-    name=f"EVTOL_quickstart_alpha{params.operating_condition.alpha}",
-)
+project.run_case(params=params, name=f"EVTOL_quickstart_alpha{params.operating_condition.alpha}")
 log.info(f"The case ID is: {project.case.id} with alpha = {params.operating_condition.alpha} ")
-
-# This line below is a simple example to show how to fork a child case from a parent case.
-project.run_case(params=params, fork_from=project.case, name="forked_case")
-
-# As a simple example of how easy it is to run multiple cases,
-# let's do some more alpha angles to create a small alpha sweep.
-alpha_list = [2, 4]
-for alpha_angle in alpha_list:
-    params.operating_condition.alpha = alpha_angle * fl.u.deg
-    project.run_case(
-        params=params, name=f"EVTOL_quickstart_alpha{params.operating_condition.alpha}"
-    )
-    log.info(f"The case ID is: {project.case.id} with alpha = {params.operating_condition.alpha} ")
