6a82274f340585cf4ac02c4640a3c3548ea1bb26
Angran Li <angran@flexcompute.com>
2025-04-11 00:34:50 +0000

    Add validation for isosurface name

--- flow360/component/simulation/validation/validation_simulation_params.py
+++ flow360/component/simulation/validation/validation_simulation_params.py
@@ -468,3 +468,23 @@ def _check_valid_models_for_liquid(models):
                 f"`{model.type}` type model cannot be used when using liquid as simulation material."
             )
     return models
+
+
+def _check_duplicate_isosurface_names(outputs):
+    if outputs is None:
+        return outputs
+    isosurface_names = []
+    for output in outputs:
+        if isinstance(output, IsosurfaceOutput):
+            for entity in output.entities.items:
+                if entity.name == "qcriterion":
+                    raise ValueError(
+                        "The name `qcriterion` is reserved for the autovis isosurface from solver, "
+                        "please rename the isosurface."
+                    )
+                if entity.name in isosurface_names:
+                    raise ValueError(
+                        f"Another isosurface with name: `{entity.name}` already exists, please rename the isosurface."
+                    )
+                isosurface_names.append(entity.name)
+    return outputs
