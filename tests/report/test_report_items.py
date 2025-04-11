f1f592b5792d1114b09e304d0cfa96f0a5ec079f
Piotr Kluba <piotr.kluba@flexcompute.com>
2025-04-11 12:58:35 +0200

    linters applied

--- tests/report/test_report_items.py
+++ tests/report/test_report_items.py
@@ -700,25 +700,20 @@ def test_2d_caption(cases):
     )
 
     chart_selected_cases = Chart2D(
-        x="total_forces/pseudo_step",
-        y="total_forces/CD",
-        select_indices=[1]
+        x="total_forces/pseudo_step", y="total_forces/CD", select_indices=[1]
     )
 
-    assert (chart_selected_cases._handle_2d_caption(x_lab='pseudo_step', y_lab='CD')
+    assert (
+        chart_selected_cases._handle_2d_caption(x_lab="pseudo_step", y_lab="CD")
         == f"{bold('CD')} against {bold('pseudo_step')} for {bold('selected cases')}."
     )
 
-    chart_items_in_row = Chart2D(
-        x="total_forces/pseudo_step",
-        y="total_forces/CD",
-        items_in_row=2
-    )
+    chart_items_in_row = Chart2D(x="total_forces/pseudo_step", y="total_forces/CD", items_in_row=2)
 
-    assert (chart_items_in_row._handle_2d_caption(x_lab='pseudo_step', y_lab='CD')
+    assert (
+        chart_items_in_row._handle_2d_caption(x_lab="pseudo_step", y_lab="CD")
         == f"{bold('CD')} against {bold('pseudo_step')}."
     )
-
 
 
 def test_3d_caption_validity(cases):
