2c69fc3cf833e957b3c8c0ddeab5485e681df408
Angran <angran@flexcompute.com>
2025-04-15 11:34:22 -0400

    [SCFD-4733] [SCFD-5021] [Hotfix-RC25.2] Get the default reference geometry settings with the provided length unit  (#892)
    * Change the unit of reference geometry in the default params
    
    * Encapsule the function to get default reference geometry
    
    * Set up default reference geometry's value if None
    
    * Update type hint for the get_default_params function
    
    * address comment

--- tests/simulation/service/test_project_util.py
+++ tests/simulation/service/test_project_util.py
@@ -1,8 +1,13 @@
+import numpy as np
 import pytest
 
-from flow360.component.project_utils import _replace_ghost_surfaces
+from flow360.component.project_utils import (
+    _replace_ghost_surfaces,
+    _set_up_default_reference_geometry,
+)
 from flow360.component.simulation.primitives import GhostSphere
 from flow360.component.simulation.simulation_params import SimulationParams
+from flow360.component.simulation.unit_system import LengthType
 
 
 @pytest.fixture(autouse=True)
@@ -17,3 +22,12 @@ def test_replace_ghost_surfaces():
     assert isinstance(entity, GhostSphere)  # This used to be GhostSurface by python
     assert entity.center == [5.0007498695, 0.0, 0.0]
     assert entity.max_radius == 504.16453591327473
+
+
+def test_set_up_default_reference_geometry():
+    params = SimulationParams.from_file("./data/simulation_with_old_ghost_surface.json")
+    length_unit = LengthType.validate("cm")
+    new_params = _set_up_default_reference_geometry(params, length_unit=length_unit)
+
+    assert np.all(new_params.reference_geometry.area == 1.0 * length_unit**2)
+    assert np.all(new_params.reference_geometry.moment_center == (0.0, 0.0, 0.0) * length_unit)
