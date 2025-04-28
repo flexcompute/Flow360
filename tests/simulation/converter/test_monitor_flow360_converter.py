5a0a4571342e97a4d98232ea5b6db5b0635bd01e
johnmoore4 <johnpmooreiv@gmail.com>
2025-04-28 08:57:39 -0400

    John/add low dissipation control factors (#958)
    * added low dissipationn control factors for internal use
    
    * format
    
    * fixed monitor_converter failing unit test
    
    ---------
    
    Co-authored-by: John Moore <john@flexcompute.com>

--- tests/simulation/converter/test_monitor_flow360_converter.py
+++ tests/simulation/converter/test_monitor_flow360_converter.py
@@ -44,7 +44,11 @@ def test_flow360_monitor_convert():
     )
     with open("./ref/ref_monitor.json", mode="r") as fp:
         ref_dict = json.load(fp=fp)
-    assert compare_values(params_dict, ref_dict, ignore_keys=["private_attribute_id"])
+    assert compare_values(
+        params_dict,
+        ref_dict,
+        ignore_keys=["private_attribute_id", "low_dissipation_control_factors"],
+    )
 
     with pytest.raises(
         ValueError,
