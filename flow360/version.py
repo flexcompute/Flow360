ece3741c2269d58a920b90da0acc6e1383ee672a
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-05-13 13:35:41 -0400

    Bump to 25.5.0 (#1039)
    * Bump to 25.5.0
    
    * Fix unit test

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.2.6"
 __solver_version__ = "release-25.2"
+=======
+__version__ = "25.5.0"
+__solver_version__ = "release-25.5"
+>>>>>>> 02ed491f (Bump to 25.5.0 (#1039))
