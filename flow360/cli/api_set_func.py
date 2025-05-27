36bafd4a10b7c570873bbdcebdc825fb70b5b013
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-05-27 11:32:13 -0400

    [FL-996] Added reloading after the configure() call to set up APIKey (#1091)
    * [FL-996] Added reloading after the configure() call to set up APIKey
    
    * Fixed Pylint
    
    * Fixed environemtn arg being dev/uat/prod

--- flow360/cli/api_set_func.py
+++ flow360/cli/api_set_func.py
@@ -2,6 +2,7 @@
 
 from click.testing import CliRunner
 
+from flow360 import user_config
 from flow360.cli.app import configure
 from flow360.log import log
 
@@ -11,12 +12,25 @@ def configure_caller(apikey: str):
     runner = CliRunner()
 
     # Construct CLI arguments as a list
+<<<<<<< HEAD
     args = ["--apikey", apikey]
+=======
+    args = ["--apikey", apikey, "--profile", profile]
+
+    if environment:
+        if environment.lower() in ("dev", "uat"):
+            args += ["--" + environment.lower()]
+        elif environment.lower() == "prod":
+            args += []
+        else:
+            args += ["--env", environment]
+>>>>>>> 49c32499 ([FL-996] Added reloading after the configure() call to set up APIKey (#1091))
 
     # Invoke the `configure` command
     result = runner.invoke(configure, args)
 
     if result.exit_code != 0:
-        log.info("Error:" + result.output)
+        log.error(result.output if result.output else str(result.exception))
     else:
         log.info("Configuration successful.")
+        user_config.UserConfig = user_config.BasicUserConfig()  # Reload
