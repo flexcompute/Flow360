2e3fbdebb9818d73cca9d181c5a14023246b7348
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-06-03 12:03:03 -0400

    [FL-58] Update Unyt package version (#1095)
    * [FL-58] Update Unyt package version
    
    * Fixed unit test

--- tests/simulation/params/test_unit_conversions.py
+++ tests/simulation/params/test_unit_conversions.py
@@ -125,3 +125,63 @@ def test_temperature_offset():
             altitude=10 * u.m, temperature_offset=11.11
         )
         assert ts.temperature_offset == 11.11 * u.K
+<<<<<<< HEAD
+=======
+
+
+def test_operations_on_units():
+
+    with SI_unit_system:
+        far_field_zone = fl.AutomatedFarfield()
+        params = fl.SimulationParams(
+            meshing=fl.MeshingParams(
+                defaults=fl.MeshingDefaults(
+                    boundary_layer_first_layer_thickness=0.001,
+                    surface_max_edge_length=1,
+                ),
+                volume_zones=[far_field_zone],
+            ),
+            reference_geometry=fl.ReferenceGeometry(),
+            operating_condition=fl.AerospaceCondition(
+                velocity_magnitude=100 * fl.u.m / 3 / fl.u.s * 7 * fl.u.inch / fl.u.cm,
+                alpha=5 * u.deg,
+            ),
+            time_stepping=fl.Steady(max_steps=1000),
+            models=[
+                fl.Wall(
+                    surfaces=[Surface(name="surface")],
+                    name="Wall",
+                ),
+                fl.Freestream(
+                    surfaces=[far_field_zone.farfield],
+                    name="Freestream",
+                ),
+            ],
+        )
+
+    replaced = params.operating_condition.velocity_magnitude * 3
+    assertions.assertAlmostEqual(replaced.value, 70000)
+    assert str(replaced.units) == "inch/s"
+
+    replaced = params.operating_condition.velocity_magnitude / (27.3 * fl.u.m / fl.u.s)
+    assertions.assertAlmostEqual(replaced.value, 21.70940170940171)
+    assert str(replaced.units) == "dimensionless"
+
+    replaced = params.operating_condition.velocity_magnitude**5 - (1 / 50 * (fl.u.km / fl.u.s) ** 5)
+    assertions.assertAlmostEqual(replaced.value, 502472105493.3395)
+    assert str(replaced.units) == "inch**5*m**5/(cm**5*s**5)"
+
+    replaced = (
+        params.operating_condition.thermal_state.temperature.to("degC") - 25 * fl.u.degC
+    ).to("K")
+    assertions.assertAlmostEqual(replaced.value, -10)
+    assert str(replaced.units) == "K"
+
+    replaced = params.operating_condition.thermal_state.temperature.to("degC") - 25 * fl.u.degC
+    assertions.assertAlmostEqual(replaced.value, -10)
+    assert str(replaced.units.expr) == "delta_degC"  # unyt 3.0+
+
+    replaced = params.operating_condition.thermal_state.density + 2 * fl.u.g / fl.u.cm**3
+    assertions.assertAlmostEqual(replaced.value, 2001.2249999999997)
+    assert str(replaced.units) == "kg/m**3"
+>>>>>>> b893c663 ([FL-58] Update Unyt package version (#1095))
