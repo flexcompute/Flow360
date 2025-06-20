7b06945412c45897c4cfb24f6d165b752a0d141d
Andrzej Krupka <andrzej@flexcompute.com>
2025-06-20 16:47:29 +0000

    Update module imports

--- flow360/__init__.py
+++ flow360/__init__.py
@@ -129,6 +129,16 @@ from flow360.component.simulation.outputs.output_entities import (
     PointArray,
     PointArray2D,
     Slice,
+    RenderCameraConfig,
+    RenderLightingConfig,
+    AmbientLight,
+    DirectionalLight,
+    OrthographicProjection,
+    PerspectiveProjection,
+    StaticCamera,
+    OrbitCamera,
+    SplineCamera,
+    KeyframeCamera
 )
 from flow360.component.simulation.outputs.outputs import (
     AeroAcousticOutput,
@@ -153,6 +163,7 @@ from flow360.component.simulation.outputs.outputs import (
     TimeAverageVolumeOutput,
     UserDefinedField,
     VolumeOutput,
+    RenderOutput
 )
 from flow360.component.simulation.primitives import (
     AxisymmetricBody,
