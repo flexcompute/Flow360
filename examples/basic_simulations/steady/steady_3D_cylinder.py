b6889411dcdbe594f2d78c5374eedbb29ebb9a23
BenYuan <ben@flexcompute.com>
2025-07-23 13:40:00 +0000

    Fix the steady-3D-cylinder example

--- examples/basic_simulations/steady/steady_3D_cylinder.py
+++ examples/basic_simulations/steady/steady_3D_cylinder.py
@@ -25,7 +25,7 @@ with fl.SI_unit_system:
         reference_geometry=fl.ReferenceGeometry(
             area=340, moment_center=[0, 0, 0], moment_length=[1, 1, 1]
         ),
-        operating_condition=fl.operating_condition_from_mach_reynolds(
+        operating_condition=fl.AerospaceCondition.from_mach_reynolds(
             reynolds=5, mach=0.1, project_length_unit=fl.u.m
         ),
         time_stepping=fl.Steady(),
