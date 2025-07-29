4860b3a32b4fa1e9316abfebf48983bf082ac583
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-07-29 11:06:52 -0400

    Removed aws_region in the model (#1302)

--- flow360/environment.py
+++ flow360/environment.py
@@ -8,7 +8,7 @@ import os
 from typing import Optional
 
 import toml
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from .file_path import flow360_dir
 from .log import log
@@ -23,13 +23,14 @@ class EnvironmentConfig(BaseModel):
     domain: str
     web_api_endpoint: str
     web_url: str
-    aws_region: str
     apikey_profile: str
     portal_web_api_endpoint: Optional[str] = None
     s3_endpoint_url: Optional[str] = None
 
+    model_config = ConfigDict(extra="ignore")
+
     @classmethod
-    def from_domain(cls, name, domain, aws_region, apikey_profile="default") -> EnvironmentConfig:
+    def from_domain(cls, name, domain, apikey_profile="default", **kwargs) -> EnvironmentConfig:
         """Create EnvironmentConfig using domain and populating to web_api_endpoint, web_url and portal_web_api_endpoint
 
         Parameters
@@ -38,8 +39,6 @@ class EnvironmentConfig(BaseModel):
             name, for example DEV
         domain : str
             domain, for example dev-simulation.cloud
-        aws_region : str
-            aws region
         apikey_profile : str, optional
             profile to be used from flow360 config for apikey, by default 'default'
 
@@ -48,13 +47,15 @@ class EnvironmentConfig(BaseModel):
         EnvironmentConfig
             completed EnvironmentConfig
         """
+        if "aws_region" in kwargs:
+            log.warning("`aws_region` is no longer a required argument. Input will be ignored.")
+
         env = cls(
             name=name,
             domain=domain,
             web_api_endpoint=f"https://flow360-api.{domain}",
             web_url=f"https://flow360.{domain}",
             portal_web_api_endpoint=f"https://portal-api.{domain}",
-            aws_region=aws_region,
             apikey_profile=apikey_profile,
         )
         return env
@@ -139,14 +140,11 @@ class EnvironmentConfig(BaseModel):
         log.info("Saved environment configuration to config.toml.")
 
 
-dev = EnvironmentConfig.from_domain(
-    name="dev", domain="dev-simulation.cloud", aws_region="us-east-1", apikey_profile="dev"
-)
-uat = EnvironmentConfig.from_domain(
-    name="uat", domain="uat-simulation.cloud", aws_region="us-west-2", apikey_profile="uat"
-)
+dev = EnvironmentConfig.from_domain(name="dev", domain="dev-simulation.cloud", apikey_profile="dev")
+uat = EnvironmentConfig.from_domain(name="uat", domain="uat-simulation.cloud", apikey_profile="uat")
 prod = EnvironmentConfig.from_domain(
-    name="prod", domain="simulation.cloud", aws_region="us-gov-west-1"
+    name="prod",
+    domain="simulation.cloud",
 )
 
 preprod = EnvironmentConfig(
@@ -155,7 +153,6 @@ preprod = EnvironmentConfig(
     web_api_endpoint="https://preprod-flow360-api.simulation.cloud",
     web_url="https://preprod-flow360.simulation.cloud",
     portal_web_api_endpoint="https://preprod-portal-api.simulation.cloud",
-    aws_region="us-gov-west-1",
     apikey_profile="default",
 )
 
