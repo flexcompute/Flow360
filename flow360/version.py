9ad52a3f3768d69a7635f02fa3a8253f3fe032cc
Angran <angran@flexcompute.com>
2025-07-29 10:44:13 -0400

    [25.5] feat(): load the storage region from the backend response (#1301)
    * load the storage region from the backend response
    
    * bump 25.5.5
    
    * Removed aws_region in the model
    
    ---------
    
    Co-authored-by: BenYuan <ben@flexcompute.com>

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.6.2b4"
 __solver_version__ = "release-25.6"
+=======
+__version__ = "25.5.5"
+__solver_version__ = "release-25.5"
+>>>>>>> 73ad176f ([25.5] feat(): load the storage region from the backend response (#1301))
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.6.2b4"
+=======
+    assert __version__ == "25.5.5"
+>>>>>>> 73ad176f ([25.5] feat(): load the storage region from the backend response (#1301))
