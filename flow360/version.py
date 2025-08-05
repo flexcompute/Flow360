8febc8d62b3e938350c80b7e673cd61785b30cd2
Ben <106089368+benflexcompute@users.noreply.github.com>
2025-08-05 16:28:02 -0400

    Bump to 25.5.6 for release (#1326)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.6.3"
 __solver_version__ = "release-25.6"
+=======
+__version__ = "25.5.6"
+__solver_version__ = "release-25.5"
+>>>>>>> a2099add (Bump to 25.5.6 for release (#1326))
