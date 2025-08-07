7696a3fcdbe6ab9bb710c24cb5066c1e73721dfb
BenYuan <ben@flexcompute.com>
2025-08-07 15:30:43 +0000

    WIP

--- tests/test_results.py
+++ tests/test_results.py
@@ -614,4 +614,10 @@ def test_surface_forces_result(mock_id, mock_response):
     ):
         surface_forces.by_body_group(params=params)
 
-    # For force distribution
+@pytest.mark.usefixtures("s3_download_override")
+def test_force_distribution_grouping(mock_id, mock_response):
+    case = fl.Case(id="case-63fd6b73-cbd5-445c-aec6-e62eca4467e6")
+    params = case.params
+    entity_info = params.private_attribute_asset_cache.project_entity_info
+    surface_forces = case.results.surface_forces
+    surface_forces_by_boundary = surface_forces.by_boundary_condition(params=params)
