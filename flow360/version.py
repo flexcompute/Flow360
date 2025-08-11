520f28828ce04f6b2f68c7eb529cfa77828262ef
BenYuan <ben@flexcompute.com>
2025-08-11 16:09:35 +0000

    Resolve conflict

--- flow360/version.py
+++ flow360/version.py
@@ -2,10 +2,5 @@
 version
 """
 
-<<<<<<< HEAD
 __version__ = "25.7.1b1"
 __solver_version__ = "release-25.7"
-=======
-__version__ = "25.6.5"
-__solver_version__ = "release-25.6"
->>>>>>> 651ac16a (Bump to 25.6.5 and also add default value application to planar tolerance (#1348))
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,8 +2,4 @@ from flow360.version import __version__
 
 
 def test_version():
-<<<<<<< HEAD
     assert __version__ == "25.7.1b1"
-=======
-    assert __version__ == "25.6.5"
->>>>>>> 651ac16a (Bump to 25.6.5 and also add default value application to planar tolerance (#1348))
