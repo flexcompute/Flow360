4bc6ee12fb30effca2190d4df3bd98699d8256ef
Maciej Skarysz <maciej@flexcompute.com>
2025-08-18 11:12:19 -0400

    bumped version for beta publish

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,5 @@
 version
 """
 
-__version__ = "25.6.5"
+__version__ = "25.8.0b1"
 __solver_version__ = "release-25.6"
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,4 @@ from flow360.version import __version__
 
 
 def test_version():
-    assert __version__ == "25.6.5"
+    assert __version__ == "25.8.0b1"
