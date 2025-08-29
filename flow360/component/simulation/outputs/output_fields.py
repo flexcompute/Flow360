34f23f4fdf18ab6211ae25accd4784eaa48db696
John Moore <john@flexcompute.com>
2025-08-29 19:36:34 +0000

    removed helicty as output option

--- flow360/component/simulation/outputs/output_fields.py
+++ flow360/component/simulation/outputs/output_fields.py
@@ -69,7 +69,6 @@ from flow360.component.simulation.unit_system import u
 # Velocity Z component (dimensioned, m/s)
 # Velocity Magnitude (dimensioned, m/s)
 # Pressure (dimensioned, Pa)
-# Helicity
 CommonFieldNames = Literal[
     "Cp",
     "Cpt",
@@ -112,7 +111,6 @@ CommonFieldNames = Literal[
     "velocity_z_m_per_s",
     "velocity_magnitude_m_per_s",
     "pressure_pa",
-    "helicity",
 ]
 
 # Skin friction coefficient vector
@@ -191,7 +189,6 @@ IsoSurfaceFieldNames = Literal[
     "velocity_x",
     "velocity_y",
     "velocity_z",
-    "helicity",
 ]
 
 AllFieldNames = Literal[CommonFieldNames, SurfaceFieldNames, VolumeFieldNames, IsoSurfaceFieldNames]
--- tests/simulation/framework/test_unique_list.py
+++ tests/simulation/framework/test_unique_list.py
@@ -126,9 +126,8 @@ def test_unique_list():
             "'velocity_x_m_per_s', "
             "'velocity_y_m_per_s', "
             "'velocity_z_m_per_s', "
-            "'velocity_magnitude_m_per_s', "
-            "'pressure_pa' "
-            "or 'helicity'"
+            "'velocity_magnitude_m_per_s' "
+            "or 'pressure_pa'"
         ),
     ):
         TempIsosurfaceOutput(isosurfaces=[my_iso_1], output_fields=["wallDistance", 1234])
