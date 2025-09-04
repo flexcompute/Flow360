f22c3fc59cdafe38a0a8723c3d65e43efdb90122
piotrkluba <piotr.kluba@flexcompute.com>
2025-09-04 13:50:09 -0400

    [HOTFIX RC 25.6]  Fix report validation recursion issue and bump to 25.6.7 (#1411)
    * Fix recursion issue (#1410)
    
    Co-authored-by: Ben <ben@flexcompute.com>
    
    * bump version to 25.6.7
    
    ---------
    
    Co-authored-by: Ben <ben@flexcompute.com>

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.7.2b1"
 __solver_version__ = "release-25.7"
+=======
+__version__ = "25.6.7"
+__solver_version__ = "release-25.6"
+>>>>>>> 398c92b8 ([HOTFIX RC 25.6]  Fix report validation recursion issue and bump to 25.6.7 (#1411))
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.7.2b1"
+=======
+    assert __version__ == "25.6.7"
+>>>>>>> 398c92b8 ([HOTFIX RC 25.6]  Fix report validation recursion issue and bump to 25.6.7 (#1411))
