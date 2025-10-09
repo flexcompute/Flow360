cf9a46f5bcd70607941513139766fbda0878927f
piotrkluba <piotr.kluba@flexcompute.com>
2025-10-09 14:53:46 +0200

    revertet some weird change to base model

--- flow360/component/simulation/framework/base_model.py
+++ flow360/component/simulation/framework/base_model.py
@@ -658,7 +658,7 @@ class Flow360BaseModel(pd.BaseModel):
                     exclude=exclude,
                     registry_lookup=registry_lookup,
                 )
-            elif isinstance(value, (list, tuple)):
+            elif isinstance(value, list):
                 # Use the helper to handle nested lists.
                 solver_values[property_name] = _preprocess_nested_list(
                     value, [loc_name], params, exclude, registry_lookup
