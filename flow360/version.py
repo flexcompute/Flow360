b6cbc357984ca4638a997f6a6131d5d3d3c426ff
Angran Li <angran@flexcompute.com>
2025-10-09 10:41:26 -0400

    [25.6] Add cap version for pydantic (#1482)
    * Add cap version for pydantic
    
    * bump solver version to 25.6.8

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.7.4b0"
 __solver_version__ = "release-25.7"
+=======
+__version__ = "25.6.8"
+__solver_version__ = "release-25.6"
+>>>>>>> 9a1318d1 ([25.6] Add cap version for pydantic (#1482))
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.7.4b0"
+=======
+    assert __version__ == "25.6.8"
+>>>>>>> 9a1318d1 ([25.6] Add cap version for pydantic (#1482))
