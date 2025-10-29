4f00dc7b8fd07a172356cf363c17a7bdedfea201
Feilin Jia <feilin@flexcompute.com>
2025-10-29 11:43:49 -0400

    remove old design code

--- flow360/component/__init__.py
+++ flow360/component/__init__.py
@@ -1,23 +1,15 @@
 """Flow360 Component Module"""
 
 from flow360.component.geometry_tree import (
-    AttributeQuery,
-    FilterExpression,
     GeometryTree,
-    Name,
     NodeType,
     TreeNode,
-    Type,
 )
 
 __all__ = [
-    "AttributeQuery",
-    "FilterExpression",
     "GeometryTree",
-    "Name",
     "NodeType",
     "TreeNode",
-    "Type",
 ]
 
 
