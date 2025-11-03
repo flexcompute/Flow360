42784bcf462eb576e7b98c49dd5aa247c1bacb87
Feilin Jia <feilin@flexcompute.com>
2025-11-03 15:05:29 -0500

    lint

--- flow360/examples/oblique_channel.py
+++ flow360/examples/oblique_channel.py
@@ -9,9 +9,7 @@ class ObliqueChannel(BaseTestCase):
     name = "obliqueChannel"
 
     class url:
-        mesh = (
-            "https://simcloud-public-1.s3.amazonaws.com/examples/obliqueChannel/cartesian_2d_mesh.oblique.cgns"
-        )
+        mesh = "https://simcloud-public-1.s3.amazonaws.com/examples/obliqueChannel/cartesian_2d_mesh.oblique.cgns"
         extra = {
             "rectangle_normal": "https://simcloud-public-1.s3.amazonaws.com/examples/obliqueChannel/rectangle_normal.cgns",
             "rectangle_oblique": "https://simcloud-public-1.s3.amazonaws.com/examples/obliqueChannel/rectangle_oblique.cgns",
