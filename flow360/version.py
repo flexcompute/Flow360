d6ef528bf1fc9c4b62d69bfc0f965a17594c190a
Angran <angran@flexcompute.com>
2025-11-12 23:05:08 +0000

    Fix unit test

--- flow360/version.py
+++ flow360/version.py
@@ -2,10 +2,5 @@
 version
 """
 
-<<<<<<< HEAD
 __version__ = "25.8.0b4"
 __solver_version__ = "release-25.8"
-=======
-__version__ = "25.7.7"
-__solver_version__ = "release-25.7"
->>>>>>> 72c81eec ([FXC-4006][FXC-4053][25.7] Add validation for transition model output fields and output frequency settings (#1587))
