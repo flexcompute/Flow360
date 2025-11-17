415649187dab212efa6718f439f0cee02aac1bd7
benflexcompute <ben@flexcompute.com>
2025-11-17 14:46:49 -0500

    Added suppression method

--- flow360/version_utils.py
+++ flow360/version_utils.py
@@ -5,7 +5,7 @@ Utilities for version handling.
 from __future__ import annotations
 
 import os
-from typing import List, Optional
+from typing import Optional
 
 from .log import log
 from .version import __version__
