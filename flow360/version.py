fb132672eca5e94ea5e3db5788d640dc807b2760
Ben <106089368+benflexcompute@users.noreply.github.com>
2026-01-10 15:08:12 -0500

    chore(): Bump to 25.6.9 (#1712)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.8.2b1"
 __solver_version__ = "release-25.8"
+=======
+__version__ = "25.6.9"
+__solver_version__ = "release-25.6"
+>>>>>>> 7b31f58b (chore(): Bump to 25.6.9 (#1712))
