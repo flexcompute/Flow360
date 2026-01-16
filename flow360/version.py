f083b632b20a8e8f02db9c3a5172d362a47cf3f7
Ben <106089368+benflexcompute@users.noreply.github.com>
2026-01-16 11:06:11 -0500

    Bump 25.7 version (#1730)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,10 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.8.3b1"
 __solver_version__ = "release-25.8"
+=======
+__version__ = "25.7.8"
+__solver_version__ = "release-25.7"
+>>>>>>> 19a9ea25 (Bump 25.7 version (#1730))
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.8.3b1"
+=======
+    assert __version__ == "25.7.8"
+>>>>>>> 19a9ea25 (Bump 25.7 version (#1730))
