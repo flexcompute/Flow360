dc429d88131c67eccd268b67d18b9fbf3e695ee0
Alex Xu <alex.xu@flexcompute.com>
2026-02-02 21:03:45 +0000

    revert accidentally added changes from other wip branch

--- flow360/component/simulation/meshing_param/volume_params.py
+++ flow360/component/simulation/meshing_param/volume_params.py
@@ -67,9 +67,9 @@ class UniformRefinement(Flow360BaseModel):
 
     name: Optional[str] = pd.Field("Uniform refinement")
     refinement_type: Literal["UniformRefinement"] = pd.Field("UniformRefinement", frozen=True)
-    entities: EntityList[Box, Cylinder, AxisymmetricBody] = pd.Field(
-        description=":class:`UniformRefinement` can be applied to :class:`~flow360.Box`, "
-        + ":class:`~flow360.Cylinder`, and :class:`~flow360.AxisymmetricBody` regions."
+    entities: EntityList[Box, Cylinder] = pd.Field(
+        description=":class:`UniformRefinement` can be applied to :class:`~flow360.Box` "
+        + "and :class:`~flow360.Cylinder` regions."
     )
     # pylint: disable=no-member
     spacing: LengthType.Positive = pd.Field(description="The required refinement spacing.")
