0d57eea1a4f9a7dd3e00b482d2597307136507a7
Mike Park <mike@flexcompute.com>
2026-02-25 20:23:09 +0000

    correctly converts geometry_accuracy into project coordinate units

--- flow360/component/simulation/meshing_param/meshing_specs.py
+++ flow360/component/simulation/meshing_param/meshing_specs.py
@@ -323,7 +323,7 @@ class MeshingDefaults(Flow360BaseModel):
         ):
             relative_bounding_box_limit = 1e-6
             bbox_diag = param_info.global_bounding_box.diagonal
-            ga_value = value.to(param_info.project_length_unit.units).value.item()
+            ga_value = (value / param_info.project_length_unit).value.item()
             lower_limit = relative_bounding_box_limit * bbox_diag
             if ga_value < lower_limit:
                 raise ValueError(
