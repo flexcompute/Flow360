cba7c6213800548c93c9cdc1f6757675e2b8c0cc
piotrkluba <piotr.kluba@flexcompute.com>
2026-02-26 14:34:29 +0000

    linter

--- flow360/component/simulation/meshing_param/meshing_validators.py
+++ flow360/component/simulation/meshing_param/meshing_validators.py
@@ -10,6 +10,7 @@ def validate_snappy_uniform_refinement_entities(refinement: UniformRefinement):
 
     Raises ValueError if any Box has a non-axis-aligned rotation or any Cylinder is hollow.
     """
+    # pylint: disable=no-member
     for entity in refinement.entities.stored_entities:
         if not isinstance(entity, (Box, Cylinder, Sphere)):
             raise ValueError(
@@ -18,16 +19,13 @@ def validate_snappy_uniform_refinement_entities(refinement: UniformRefinement):
             )
         if (
             isinstance(entity, Box)
-            and entity.angle_of_rotation.to("deg") % (360 * u.deg)
-            != 0 * u.deg  # pylint: disable=no-member
+            and entity.angle_of_rotation.to("deg") % (360 * u.deg) != 0 * u.deg
         ):
             raise ValueError(
                 "UniformRefinement for snappy accepts only Boxes with axes aligned"
                 + " with the global coordinate system (angle_of_rotation=0)."
             )
-        if (
-            isinstance(entity, Cylinder) and entity.inner_radius.to("m") != 0 * u.m
-        ):  # pylint: disable=no-member
+        if isinstance(entity, Cylinder) and entity.inner_radius.to("m") != 0 * u.m:
             raise ValueError(
                 "UniformRefinement for snappy accepts only full cylinders (where inner_radius = 0)."
             )
