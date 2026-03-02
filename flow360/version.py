1e56eafb7e4feeb3d367a61554ddd53c4637d882
Ben <106089368+benflexcompute@users.noreply.github.com>
2026-03-02 15:37:01 -0500

    chore(): Bump to 25.8.6 (#1861)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,9 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.9.1b1"
+=======
+__version__ = "25.8.6"
+>>>>>>> 24f0a9f2 (chore(): Bump to 25.8.6 (#1861))
 __solver_version__ = "release-25.8"
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.9.1b1"
+=======
+    assert __version__ == "25.8.6"
+>>>>>>> 24f0a9f2 (chore(): Bump to 25.8.6 (#1861))
