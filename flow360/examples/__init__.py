a571646319444a78acd2b0c792c3675c4a28e072
marcvivesmassana <marc.vives@flexcompute.com>
2026-03-03 15:34:06 +0100

    new geometry

--- flow360/examples/__init__.py
+++ flow360/examples/__init__.py
@@ -8,6 +8,7 @@ from .cube import Cube
 from .cylinder2D import Cylinder2D
 from .cylinder3D import Cylinder3D
 from .DARPA_SUBOFF import DARPA_SUBOFF
+from .DTU_WindTurbine import DTU_WindTurbine
 from .drivaer import DrivAer
 from .evtol import EVTOL
 from .f1_2025 import F1_2025
@@ -43,6 +44,7 @@ __all__ = [
     "Cylinder2D",
     "Cylinder3D",
     "DARPA_SUBOFF",
+    "DTU_WindTurbine",
     "DrivAer",
     "EVTOL",
     "F1_2025",
