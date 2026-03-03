a43d5179e230a715a50b920d68d00565550a3105
Ben <106089368+benflexcompute@users.noreply.github.com>
2026-03-03 16:13:06 -0500

    chore(): update version to 25.8.7 across all relevant files (#1875)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,9 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.9.2b1"
+=======
+__version__ = "25.8.7"
+>>>>>>> 9446875b (chore(): update version to 25.8.7 across all relevant files (#1875))
 __solver_version__ = "release-25.8"
--- tests/test_current_flow360_version.py
+++ tests/test_current_flow360_version.py
@@ -2,4 +2,8 @@ from flow360.version import __version__
 
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "25.9.2b1"
+=======
+    assert __version__ == "25.8.7"
+>>>>>>> 9446875b (chore(): update version to 25.8.7 across all relevant files (#1875))
