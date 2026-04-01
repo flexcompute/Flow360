0b854ce92fedc19353192d7efbad39943213cedc
piotrkluba <piotr.kluba@flexcompute.com>
2026-04-01 12:20:11 +0000

    linter

--- flow360/component/simulation/meshing_param/snappy/snappy_params.py
+++ flow360/component/simulation/meshing_param/snappy/snappy_params.py
@@ -59,8 +59,9 @@ class SurfaceMeshingParams(Flow360BaseModel):
                 if refinement.min_spacing is None and refinement.proximity_spacing:
                     if refinement.proximity_spacing > self.defaults.min_spacing:
                         log.warning(
-                            f"Proximity spacing ({refinement.proximity_spacing}) was set higher than the minimal spacing "
-                            + f"({self.defaults.min_spacing}), setting proximity spacing to minimal spacing."
+                            f"Proximity spacing ({refinement.proximity_spacing}) was set higher "
+                            + f"than the minimal spacing ({self.defaults.min_spacing}), "
+                            + "setting proximity spacing to minimal spacing."
                         )
                         refinement.proximity_spacing = self.defaults.min_spacing
 
