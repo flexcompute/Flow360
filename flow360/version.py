90b8ae8005f219e0a07d70a7f58bd2dd5f3bb6c6
Ben <106089368+benflexcompute@users.noreply.github.com>
2026-05-05 13:34:25 -0400

    Bump 25. 9 version to 25.9.7 (#2035)

--- flow360/version.py
+++ flow360/version.py
@@ -2,5 +2,9 @@
 version
 """
 
+<<<<<<< HEAD
 __version__ = "25.10.0b1"
+=======
+__version__ = "25.9.7"
+>>>>>>> 89a19ccb (Bump 25. 9 version to 25.9.7 (#2035))
 __solver_version__ = "release-25.9"
