f5b36f122d3eff8ced348d07d30983832bd45454
piotrkluba <piotr.kluba@flexcompute.com>
2026-05-13 09:07:18 +0000

    [FXC-7659] Added user guide ref to run control docstrings (#2042)

--- flow360/component/simulation/run_control/run_control.py
+++ flow360/component/simulation/run_control/run_control.py
@@ -2,4 +2,37 @@
 
 # pylint: disable=unused-import
 
+<<<<<<< HEAD
 from flow360_schema.models.simulation.run_control.run_control import RunControl
+=======
+import pydantic as pd
+
+from flow360.component.simulation.framework.base_model import Flow360BaseModel
+from flow360.component.simulation.run_control.stopping_criterion import (
+    StoppingCriterion,
+)
+
+
+class RunControl(Flow360BaseModel):
+    """
+    :class:`RunControl` class for run control settings.
+    For general overview see :ref:`Run Control <runControl>`.
+
+    Example
+    -------
+
+    >>> criterion = fl.StoppingCriterion(...)
+    >>> fl.RunControl(
+    ...     stopping_criteria = [criterion],
+    ... )
+
+    ====
+    """
+
+    stopping_criteria: Optional[List[StoppingCriterion]] = pd.Field(
+        None,
+        description="A list of :class:`StoppingCriterion` for the solver. "
+        "All criteria must be met at the same time to stop the solver.",
+    )
+    type_name: Literal["RunControl"] = pd.Field("RunControl", frozen=True)
+>>>>>>> 5fb255cd ([FXC-7659] Added user guide ref to run control docstrings (#2042))
